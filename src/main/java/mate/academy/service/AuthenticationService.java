28f9ce03cc5a2994a12d12680fffba9ddcf36379
Bohdan Chupika <boroda4436@users.noreply.github.com>
2021-06-30 16:45:37 +0300

    Update src/main/java/mate/academy/service/AuthenticationService.java
    Co-authored-by: RomanDubovskyi <58385506+RomanDubovskyi@users.noreply.github.com>

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,7 +2,7 @@ package mate.academy.service;
 
 public class AuthenticationService {
     /**
-     * Imagine that some user wan't to login to your site.
+     * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
      * @param email - user's email
