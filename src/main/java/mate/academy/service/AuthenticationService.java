3b9a536f3e7f853c6450d8140889e7566b56e4a7
Bohdan Chupika <chupika.bohdan@ukr.net>
2021-06-30 16:14:57 +0300

    added requirements

---
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -0,0 +1,16 @@
+package mate.academy.service;
+
+public class AuthenticationService {
+    /**
+     * Imagine that some user wan't to login to your site.
+     * You should check if user credentials (login and password) are valid or not.
+     * All users are stored in <code>UserService</code> class.
+     * @param email - user's email
+     * @param password - user's password
+     * @return true if user by email exists and passed password is equals to user's password.
+     * Return false in any other cases.
+     */
+    public boolean login(String email, String password) {
+        return false;
+    }
+}
