c4dd3a3efdf5b8b37d15f3907cd93e1ea56ae601
Kostia Dementiev <konstde00@gmail.com>
2021-07-17 20:25:35 +0300

    added solution

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        if (UserService.findByEmail(email) == null) {
+            return false;
+        } else if ((UserService.findByEmail(email).getPassword()).equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
