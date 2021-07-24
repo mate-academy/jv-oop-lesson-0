5d4742579638daa8622f7d298b5c8542c66d1558
mishamolnar <michaellmuller423@gmail.com>
2021-07-24 09:59:20 +0300

    login method and iimported static method getlogin

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,9 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
+import static mate.academy.service.UserService.findByEmail;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +15,11 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        String realPassword = findByEmail(email).getEmail();
+        if (realPassword.equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -5,8 +5,7 @@ import mate.academy.model.User;
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
-            new User("alice@i.ua", "1234")
-    };
+            new User("alice@i.ua", "1234")};
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
@@ -14,7 +13,7 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+    public static User findByEmail(String email) {
         for (User name: users) {
             if ( email.equals(name.getEmail())) {
                 return name;
@@ -22,4 +21,5 @@ public class UserService {
         }
         return null;
     }
+
 }
