f2bb83e19a57849cf8d31b805e23eae63cb4020d
dogovor <dogovor@lan.com.ua>
2021-08-12 18:39:55 +0300

    chenged method login

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,19 +2,19 @@ package mate.academy.service;
 
 public class AuthenticationService {
     /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+       * Imagine that some user wants to login to your site.
+       * You should check if user credentials (login and password) are valid or not.
+       * All users are stored in <code>UserService</code> class.
+       *
+       * @param email    - user's email
+       * @param password - user's password
+       * @return true if user by email exists and passed password is equal to user's password.
+       * Return false in any other cases.
+       */
     public boolean login(String email, String password) {
         UserService userService = new UserService();
-        if (userService.findByEmail(email) != null) {
-            return password.equals(userService.findByEmail(email).getPassword());
-        }
-        return false;
+        return userService.findByEmail(email) != null
+                    && userService.findByEmail(email).getPassword().equals(password);
     }
 }
+
