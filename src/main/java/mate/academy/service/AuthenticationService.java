67fc3bf3cd85fccdb3c6b75660a656fa7ccc6fd0
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-08-12 11:11:30 +0300

    add login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,11 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService user = new UserService();
+        if (user.findByEmail(email) != null
+                && user.findByEmail(email).getPassword().equals(password)) {
+            return true;
+        }
         return false;
     }
 }
