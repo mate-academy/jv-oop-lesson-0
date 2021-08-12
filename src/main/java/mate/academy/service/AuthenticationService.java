333d93d1178872e2785f8614744d10291220f88f
Kostia Dementiev <konstde00@gmail.com>
2021-08-12 22:44:08 +0300

    fixed mistakes in solution

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,12 +13,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        if (UserService.findByEmail(email) == null) {
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+        if (user == null) {
             return false;
-        } else if ((UserService.findByEmail(email).getPassword()).equals(password)) {
-            return true;
         } else {
-            return false;
+            return password.equals(user.getPassword());
         }
     }
 }
