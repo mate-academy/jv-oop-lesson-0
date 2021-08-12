32504be5e0c866fdb40d2e00674a987a253baf02
SolnalarmJulia <solnalarm@meta.ua>
2021-08-12 22:57:27 +0300

    login method in AuthenticationService; findByEmail in UserService with correct style

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,19 +7,23 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        if(email == null || password == null)
+        if (email == null || password == null) {
             return false;
-        if(email.isEmpty() || password.isEmpty())
-        return false;
-        for (User user : UserService.users){
-            if(user.getEmail().equals(email) && user.getPassword().equals(password))
+        }
+        if (email.isEmpty() || password.isEmpty()) {
+            return false;
+        }
+        for (User user : UserService.users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
                 return true;
+            }
         }
         return false;
     }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -17,8 +17,9 @@ public class UserService {
      */
     public User findByEmail(String email) {
         for (User user : users) {
-            if (user.getEmail().equals(email))
+            if (user.getEmail().equals(email)) {
                 return user;
+            }
         }
         return null;
     }
