f9c055109ad5431761a43b5e988e4fcd2b761cc5
DenysST <85154693+DenysST@users.noreply.github.com>
2021-08-12 15:34:44 +0300

    Renamed uninformative variable

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,9 +15,9 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        for (User i : users) {
-            if (i.getEmail().equals(email)) {
-                return i;
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
             }
         }
         return null;
