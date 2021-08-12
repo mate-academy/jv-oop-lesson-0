021021a5a6c9df3abeaee72f55cb410ca497e1bf
Andruwa808 <legacy112233@gmail.com>
2021-08-12 18:02:32 +0300

    fix checkstyle

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -19,7 +19,7 @@ public class UserService {
             return null;
         }
         for (int i = 0; i < users.length; i++) {
-             if (email.equals(users[i].getEmail())) {
+            if (email.equals(users[i].getEmail())) {
                 return users[i];
             }
         }
