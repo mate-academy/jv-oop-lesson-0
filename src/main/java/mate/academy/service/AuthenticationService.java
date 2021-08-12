070eab815d405e0ff3c9685ba7c143adfa19cd0d
Nazar Hetun <nazarhetun@gmail.com>
2021-08-12 20:51:45 +0300

    fixed login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,11 +11,11 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService us = new UserService();
-        if (us.findByEmail(email) != null) {
-            return us.findByEmail(email).getPassword().equals(password);
+        UserService userService = new UserService();
+        if (userService.findByEmail(email) == null) {
+            return false;
+        } else {
+            return userService.findByEmail(email).getPassword().equals(password);
         }
-        return false;
     }
-
 }
