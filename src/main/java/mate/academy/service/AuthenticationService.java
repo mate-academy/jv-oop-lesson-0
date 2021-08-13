0e614fccd3c657f397689bb0a36b0ad41dda6ba7
Kostia Dementiev <konstde00@gmail.com>
2021-08-13 15:15:01 +0300

    fixed possible NullPointerException

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,10 +15,9 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        if (user == null) {
-            return false;
-        } else {
+        if (user != null) {
             return password.equals(user.getPassword());
         }
+        return false;
     }
 }
