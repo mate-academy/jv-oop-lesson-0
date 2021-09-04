7c2902a74ff2362493fdafe0acb8e414f3590c9c
Roman Polulikh <polilikh.r@gmail.com>
2021-09-04 14:13:05 +0300

    implemented findByEmail and login methods

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,12 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        User userByEmail = null;
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                userByEmail = user;
+            }
+        }
+        return userByEmail;
     }
 }
