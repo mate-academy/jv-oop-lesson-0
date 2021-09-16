a4570778bbb78b131faa2359038eb5110f21e923
Oleg Shiriaev <mystidreamer@gmail.com>
2021-09-16 20:20:13 +0300

    login method was fixed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,20 +1,15 @@
 package mate.academy.service;
 
-public class AuthenticationService extends UserService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+import mate.academy.model.User;
+
+public class AuthenticationService {
+    UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        if (findByEmail(email) == null) {
+        User user = userService.findByEmail(email);
+        if (user == null) {
             return false;
         }
-        return email.equals(findByEmail(email).getEmail())
-                && password.equals(findByEmail(email).getPassword());
+        return password.equals(user.getPassword());
     }
 }
