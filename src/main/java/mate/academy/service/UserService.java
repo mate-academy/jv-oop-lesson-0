e6f94d1120fe54c68f8e374a18a7be8d419b0449
Volodimir <vova.stepanenko2020@gmail.com>
2021-10-07 04:45:05 +0300

    checkstyle fix

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,24 +1,24 @@
 package mate.academy.service;
 
+import java.util.Arrays;
 import mate.academy.model.User;
 
-import java.util.Arrays;
-
-
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public static User findByEmail(String email) {
-        User user = Arrays.stream(users).filter(findUser -> findUser.getEmail().equals(email)).findFirst().orElse(null);
+        User user = Arrays.stream(users).filter(findUser -> findUser.getEmail().equals(email))
+                .findFirst().orElse(null);
         return user != null ? user : new User("none", "none");
     }
 }
