abd52bcfa284b1ba33142dc3f2de373b1cb3f10e
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-10-12 20:47:49 +0300

    second quickfix

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,6 +13,7 @@ public class AuthenticationService {
 
     public boolean login(String email, String password) {
         if (email == null || password == null) { return false; }
+        if (UserService.findByEmail(email) == null) { return false; }
         if (UserService.findByEmail(email).getPassword().equals(password)) { return true; }
         return false;
     }
