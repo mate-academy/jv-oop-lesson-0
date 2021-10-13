7afdd7c9b4494ecb17984dfcfdd7a3c1c98259ff
Zachar Chorny <ufumoveldar@gmail.com>
2021-10-13 17:19:59 +0300

    new edits(2)

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,11 +13,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService check = new UserService();
-        User user = check.findByEmail(email);
-        if (user != null) {
-            return user.getEmail().equals(email) && user.getPassword().equals(password);
-        }
-        return false;
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static String eparametr = "[A-Za-z].*?@i\\.ua";
+    private final static String EMAIL_MATCHER = "[A-Za-z].*?@i\\.ua";
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
@@ -16,14 +16,13 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        User user = null;
-        if (email.matches(eparametr)) {
-            for (User users : users) {
-                if (users.getEmail() == email) {
-                    user = users;
+        if (email.matches(EMAIL_MATCHER)) {
+            for (User user : users) {
+                if (user.getEmail() == email) {
+                    return user;
                 }
             }
         }
-        return user;
+        return null;
     }
 }
