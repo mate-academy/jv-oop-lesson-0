40cc8a524a351244c9aa8ca8777443ed914f059d
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-10-13 20:28:04 +0300

    fixed style

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,6 +13,7 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
