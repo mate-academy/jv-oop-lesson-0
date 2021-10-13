be0422a88ed40d7fe91fcd960404df5461ad98b0
Zachar Chorny <ufumoveldar@gmail.com>
2021-10-13 17:35:08 +0300

    new edits(3)

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private final static String EMAIL_MATCHER = "[A-Za-z].*?@i\\.ua";
+    private static final String EMAIL_MATCHER = "[A-Za-z].*?@i\\.ua";
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
