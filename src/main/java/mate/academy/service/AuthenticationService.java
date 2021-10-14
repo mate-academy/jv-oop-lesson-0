302a2eff7a7fd9ee20919a37d30ea170bf767d74
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-10-14 11:43:12 +0300

    created an object of UserService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,8 +12,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        User user = UserService.findByEmail(email);
+        User user = userService.findByEmail(email);
         return user != null && password.equals(user.getPassword());
     }
 }
