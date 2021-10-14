e181361eff0dd409811ef9628241a40872b3590a
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-10-14 12:21:50 +0300

    practice with OOP

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -2,6 +2,7 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
+
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
