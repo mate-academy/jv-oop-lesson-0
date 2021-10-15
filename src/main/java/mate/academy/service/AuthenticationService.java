31216a026d7a1049c1b61c1bb6adba873a01dab1
ZhukovVladyslav <68158424+ZhukovVladyslav@users.noreply.github.com>
2021-10-15 15:16:58 +0300

    Update AuthenticationService.java

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,7 +1,6 @@
 package mate.academy.service;
 
 public class AuthenticationService {
-
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         if (userService.findByEmail(email) != null
