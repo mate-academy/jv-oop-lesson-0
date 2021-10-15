cff94b38e5a405bba4a340f5d4fe2f2c97e0800b
ZhukovVladyslav <68158424+ZhukovVladyslav@users.noreply.github.com>
2021-10-15 14:38:05 +0300

    login metod

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,12 +3,11 @@ package mate.academy.service;
 public class AuthenticationService {
 
     public boolean login(String email, String password) {
-        boolean b = false;
         UserService userService = new UserService();
         if (userService.findByEmail(email) != null
                 && userService.findByPassword(password) != null) {
-            b = true;
+            return true;
         }
-        return b;
+        return false;
     }
 }
