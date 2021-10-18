2aeefa329b574cd28f022ef2a7eb5479e518154a
unknown <vladzhukov9389@gmail.com>
2021-10-18 05:46:38 +0300

    I hope this will work

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -4,7 +4,7 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         if (userService.findByEmail(email) != null
-                && userService.findByEmail(email).getPassword() == password) {
+                && userService.findByEmail(email).getPassword().equals(password)) {
             return true;
         }
         return false;
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -10,7 +10,7 @@ public class UserService {
 
     public User findByEmail(String email) {
         for (User user : users) {
-            if (email == user.getEmail()) {
+            if (email.equals(user.getEmail())) {
                 return user;
             }
         }
