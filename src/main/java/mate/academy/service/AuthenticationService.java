322766c1c5a307c37389510ccfa8e5b7f7150572
unknown <vladzhukov9389@gmail.com>
2021-10-18 17:38:31 +0300

    one more time)

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -6,9 +6,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User ourUser = userService.findByEmail(email);
-        if (ourUser != null && ourUser.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        return ourUser != null && ourUser.getPassword().equals(password);
     }
 }
