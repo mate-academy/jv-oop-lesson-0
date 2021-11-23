42c91812c78ef20d0dfc4917616193d06fc4e991
resci <vlad.evdockimov.062@gmail.com>
2021-11-23 08:38:11 +0200

    implemented findByEmail method in UserService

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,12 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        User resultUser = null;
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                resultUser = user;
+            }
+        }
+        return resultUser;
     }
 }
