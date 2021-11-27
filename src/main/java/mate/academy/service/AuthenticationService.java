c2ead98ceb114c772363d7f718e21ba5ce30bb4f
AprioriKS <sounddummies@gmail.com>
2021-11-27 21:11:40 +0200

    code reformated and static import fixed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,29 +2,24 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
-import static mate.academy.service.UserService.findByEmail;
-
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
 
-        User checkUser = findByEmail(email);
+        User checkUser = UserService.findByEmail(email);
         if (checkUser == null) {
             return false;
         }
-        if (checkUser.getEmail().equals(email) && checkUser.getPassword().equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
+        return checkUser.getEmail().equals(email) && checkUser.getPassword().equals(password);
 
     }
 }
