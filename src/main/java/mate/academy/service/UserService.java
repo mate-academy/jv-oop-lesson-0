98a879c5b175edbe1fbf0b28b43907a29883e9b4
Victor Rudkovsky <vpcomtek@gmail.com>
2021-12-30 00:07:38 +0200

    task solution

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -2,12 +2,13 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
+import java.util.Arrays;
+
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
-
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      * @param email - the input parameter
@@ -15,6 +16,8 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        return Arrays.stream(users).filter(u -> u.getEmail().equals(email))
+                .findAny()
+                .orElse(null);
     }
 }
