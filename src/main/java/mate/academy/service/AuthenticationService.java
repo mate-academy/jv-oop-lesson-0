278923ec1281dd4c209cb73d1d0c6f584d60c477
yuriikozar <yuriikozar@gmail.com>
2022-02-16 23:28:51 +0200

    AuthenticationService Added

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,9 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -10,7 +14,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    UserService u = new UserService();
+
     public boolean login(String email, String password) {
+        if (u.findByEmail(email) !=null&&(u.findByEmail(email)).getPassword().equals(password)) return  true;
         return false;
     }
 }
