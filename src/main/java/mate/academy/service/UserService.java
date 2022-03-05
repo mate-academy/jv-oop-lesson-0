3ede725f1da68d5a50fe91ae2a1655d97fcd4085
Dmytro Koliada <dmitrikstar@gmail.com>
2022-03-05 19:57:03 +0200

    implemented_task

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -5,7 +5,9 @@ import mate.academy.model.User;
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
-            new User("alice@i.ua", "1234")
+            new User("alice@i.ua", "1234"),
+            new User("john@i.ua", "1234"),
+            new User("bob@i.ua", "qwerty")
     };
 
     /**
