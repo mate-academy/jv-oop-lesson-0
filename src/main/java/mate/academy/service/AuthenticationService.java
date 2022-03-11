ce7a3b2b4cfebdcb715fb4bc49edb60b6eb8ea31
Vladyslav Lyakhin <kapuuustin@gmail.com>
2022-03-11 21:34:29 +0200

    Complete a task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,7 +7,7 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     * @param email - user's ema il
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -11,7 +11,7 @@ public class UserService {
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
+     * @return - user if his  email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public static User findByEmail(String email) {
