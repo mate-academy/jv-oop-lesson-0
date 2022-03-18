91adf765ea4ed0f14364d4470cf5e83db3f18810
Yaroslav Lapovets <lapovetsyaroslav@gmail.com>
2022-03-18 08:54:54 +0200

    Add user authentication and email search

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,16 +1,16 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
     public boolean login(String email, String password) {
+        UserService user = new UserService();
+        User auth = user.findByEmail(email);
+        if (auth != null) {
+            if (email.equals(auth.getEmail())) {
+                return password.equals(auth.getPassword());
+            }
+        }
         return false;
     }
 }
