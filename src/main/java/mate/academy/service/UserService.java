d882b386b37abb14462dcf6073b7374fad208da7
Anton Karpov <antonkarpov547@gmail.com>
2022-04-03 13:57:45 +0300

    --amend

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -5,8 +5,6 @@ import mate.academy.model.User;
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
-            new User("bob@i.ua", "qwerty"),
-            new User("john@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
