6933bd2c45b055d0ba69d522409f25163aee0472
Anton Karpov <antonkarpov547@gmail.com>
2022-04-03 13:52:52 +0300

    implemented task

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -5,6 +5,8 @@ import mate.academy.model.User;
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
+            new User("bob@i.ua", "qwerty"),
+            new User("john@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
@@ -15,6 +17,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User user:users) {
+            if (user.getEmail() == email) {
+                return user;
+            }
+        }
         return null;
     }
 }
