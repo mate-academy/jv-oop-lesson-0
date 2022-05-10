d44237a92e7bf7027a4f77a8467290e5f101ca08
Dima Podakin <calombo.podakin@ukr.net>
2022-05-10 22:57:52 +0300

    implemented login

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,10 +3,12 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    public static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
+
+
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
@@ -15,6 +17,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User user : users) {
+            if (email.equals(user.getEmail())) {
+                return user;
+            }
+        }
         return null;
     }
 }
