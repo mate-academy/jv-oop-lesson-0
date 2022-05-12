7e00267601c1b854de08381f11f2faa0c386d8d2
TymurTsvietkov <tymur.tsvietkov@gmail.com>
2022-05-12 22:25:15 +0300

    implemented task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,16 +1,28 @@
 package mate.academy.service;
 
-public class AuthenticationService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+import mate.academy.model.User;
+
+public class AuthenticationService extends UserService {
+
     public boolean login(String email, String password) {
-        return false;
+        UserService userData = new UserService();
+        User userAuthentication = userData.findByEmail(email);
+        if (userAuthentication != null) {
+            return email.equals(userAuthentication.getEmail())
+                    && password.equals(userAuthentication.getPassword());
+        } else {
+            return false;
+        }
     }
 }
+
+/**
+ * Imagine that some user wants to login to your site.
+ * You should check if user credentials (login and password) are valid or not.
+ * All users are stored in <code>UserService</code> class.
+ * @param email - user's email
+ * @param password - user's password
+ * @return true if user by email exists and passed password is equal to user's password.
+ * Return false in any other cases.
+ */
+
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,20 +1,28 @@
+/**
+ * Find user by email. All users are stored in <code>private static final User[] users</code>
+ * @param email - the input parameter
+ * @return - user if his email is equal to passed email.
+ * Return <code>null</code> if there is no suitable user
+ */
+
 package mate.academy.service;
 
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+
+    public static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
-    /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
-     */
     public User findByEmail(String email) {
-        return null;
+        User findUser = null;
+        for (User userData : users) {
+            if (email.equals(userData.getEmail())) {
+                findUser = userData;
+            }
+        }
+        return findUser;
     }
 }
