0f1f8cd079a98aed24ef0de0e4442aff7f3f72e8
Vasyl Bevz <104630330+Vasyl-Bevz@users.noreply.github.com>
2022-05-13 14:35:30 +0300

    Update AuthenticationService.java

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,7 +13,7 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userChecking = new UserService();
         if (userChecking.findByEmail(email) != null 
-            && userChecking.findByEmail(email).getPassword() == password) {
+                && userChecking.findByEmail(email).getPassword() == password) {
             return true;
         }
         return false;
