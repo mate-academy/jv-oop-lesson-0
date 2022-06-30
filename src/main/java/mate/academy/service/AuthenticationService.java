0da37cec4891efd72dfc34dbdb28887715005cdf
ArturZhadan <artur.zhadan.17@gmail.com>
2022-07-01 00:46:34 +0300

    implemented login and findByEmail methods

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,15 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+        boolean flag = false;
+        if (user == null) {
+            return flag;
+        }
+        if (password.equals(user.getPassword())) {
+            flag = true;
+        }
+        return flag;
     }
 }
