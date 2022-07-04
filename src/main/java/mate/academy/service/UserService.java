ac858bd4363faa69ae2bcfb00e74ae4098b719c7
Alexandr Semashko <semashko123456789@gmail.com>
2022-07-04 19:31:59 +0300

    implements two methods

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User u : users) {
+            if(email.equals(u.getEmail())){
+                return u;
+            }
+        }
         return null;
     }
 }
