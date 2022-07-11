ac89107649e61da622e7a1d64a2949bc902f1c09
Sofiia <sofiakazzz@gmail.com>
2022-07-11 22:30:49 +0300

    implemented login and findByEmail method

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,12 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for(User umail : users) {
+            if (umail.getEmail().equals(email)) {
+                return umail;
+            }
+        }
+
         return null;
     }
 }
