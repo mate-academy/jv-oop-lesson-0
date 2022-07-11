8b1a01bb1603b845fc5e406ff66955461f107904
Sofiia <sofiakazzz@gmail.com>
2022-07-11 23:22:58 +0300

    implemented login method

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User userE : users) {
+            if (userE.getEmail().equals(email)) {
+                return userE;
+            }
+        }
         return null;
     }
 }
