7ae39480af5e56ad035b439be837920649d9910e
Misha Brovkin <haasiakira@gmail.com>
2022-07-13 11:50:17 +0300

    implemented task

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -14,7 +14,12 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+    public static User findByEmail(String email) {
+        for (User currentUser : users) {
+            if (currentUser.getEmail().equals(email)) {
+                return currentUser;
+            }
+        }
         return null;
     }
 }
