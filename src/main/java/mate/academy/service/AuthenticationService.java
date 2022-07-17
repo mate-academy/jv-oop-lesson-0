25441e5d5994fa16e0f728ea8d1a52d6d8f52319
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2022-07-17 18:21:43 +0300

    implemented task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -10,7 +10,16 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
+    private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        return false;
+        if (userService.findByEmail(email) == null) {
+            return false;
+        } else if (password.equals(userService.findByEmail(email).getPassword())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
