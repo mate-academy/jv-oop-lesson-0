edc5dde0d6fd22c1d53b5ced15a7a1ed6ec857f9
max-bolotin <realprof.mx@gmail.com>
2022-07-20 11:00:56 +0300

    If/else statement corrected, else deleted

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -18,8 +18,7 @@ public class AuthenticationService {
         User user = userService.findByEmail(email);
         if (user == null) {
             return false;
-        } else {
-            return user.getPassword().equals(password);
         }
+        return user.getPassword().equals(password);
     }
 }
