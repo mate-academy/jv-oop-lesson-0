af6ac81a9e33f1908e57431fafcc565ac426e100
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-07-20 08:46:20 +0300

    Use non-static methods, a few minor fixes

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,18 +7,14 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     *
-     * @param email    - user's email
+     * @param email - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService user  = new UserService();
-        User findEmail = user.findByEmail(email);
-        if (findEmail != null) {
-            return user.findByEmail(email).getEmail().equals(password);
-        }
-        return false;
+        UserService userService = new UserService();
+        User userFoundByEmail = userService.findByEmail(email);
+        return userFoundByEmail != null && userFoundByEmail.getEmail().equals(password);
     }
 }
