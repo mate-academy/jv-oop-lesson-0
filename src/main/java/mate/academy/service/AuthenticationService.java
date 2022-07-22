373a31eef22d0001f88276e38f9db82036ed5482
tort <dsdd>
2022-07-22 17:09:32 +0200

    Done

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -6,7 +6,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        return user == null ? false :
-                       user.getPassword().equals(password) && user != null ? true : false;
+        return user == null ? false : user.getPassword().equals(password);
     }
 }
