0dfaf408c2db3400304742db06b74432943f11c4
EagleAndrew <109113005+EagleAndrew@users.noreply.github.com>
2022-07-22 03:54:10 +0200

    Update AuthenticationService.java

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -6,12 +6,13 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService newName = new UserService();
         User userAvail = newName.findByEmail(email);
-        if (userAvail == null) {
+        return userAvail.getPassword().equals(password) && userAvail != null ? true : false;
+        /* if (userAvail == null) {
             return false;
         }
         if (userAvail.getPassword().equals(password)) {
             return true;
-        }
+        } */
         return false;
     }
 }
