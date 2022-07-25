260bbf54b918f0fa229aca300d45c65f9f54ca2f
Ilja Nekrituk <Nekrit99@gmail.com>
2022-07-25 15:43:50 +0300

    Add all changes to compile file

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -4,9 +4,9 @@ import mate.academy.model.User;
 
 public class UserService {
     private static final User[] users = new User[] {
-            new User("bob@i.ua", "1234");
-            new User("alice@i.ua", "1234");
-    }
+            new User("bob@i.ua", "1234"),
+            new User("alice@i.ua", "1234")
+    };
 
     public User findByEmail(String email) {
         for (User user : users) {
