7d2dea0a3169283d11bc0c9034f72504b52793cf
UmyaFamilia <umyafamilya1974@gmail.com>
2022-07-27 00:16:24 +0300

    lesson0

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,10 +13,15 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        if (userService.findByEmail(email) != null) {
-            return userService.findByEmail(email).getPassword().equals(password);
+       UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+        if (user != null) {
+           return user.getPassword().equals(password);
         }
+//        if (userService.findByEmail(email) != null) {
+//            return userService.findByEmail(email).getPassword().equals(password);
+//        }
+//        return false;
         return false;
     }
 }
