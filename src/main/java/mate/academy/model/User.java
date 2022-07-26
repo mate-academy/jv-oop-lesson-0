b034987284cb6a62207796279d90c0d2f4a1760c
UmyaFamilia <umyafamilya1974@gmail.com>
2022-07-27 00:06:20 +0300

    lesson0

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -8,9 +8,11 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getPassword() {
         return password;
     }
