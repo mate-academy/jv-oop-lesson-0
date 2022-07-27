825a425735c7836198368bb7559660f324a79a40
Veers <raadch3011@gmail.com>
2022-07-27 12:12:36 +0300

    deleted comments, use findByMail method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,15 +1,15 @@
 package mate.academy.service;
 
+import java.util.Objects;
 import mate.academy.model.User;
 
-import java.util.Objects;
 
 public class AuthenticationService {
 
     public boolean login(String email, String password) {
         User user = new UserService().findByEmail(email);
 
-        if  (user != null) {
+        if (user != null) {
             return Objects.equals(password, user.getPassword());
         }
 
