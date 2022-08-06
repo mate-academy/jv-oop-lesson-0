6b4156cfec877d53246e99eaa0107d0e46bf6854
Oleg Marchuk <mar4uk14@gmail.com>
2022-08-06 11:02:56 +0300

    implemented login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,9 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -11,6 +14,14 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        for (User user : UserService.getUsers()) {
+            if (email.equals(user.getEmail())) {
+                if (password.equals(user.getPassword())) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
