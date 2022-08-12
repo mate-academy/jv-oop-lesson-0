b059ab5501ec9b2400f3c9ead2f0f2c33985ed7f
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-08-12 18:46:31 +0300

    Sec attempt without maven error

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,13 +1,10 @@
 package mate.academy.service;
 
 public class AuthenticationService {
-    private static UserService findbyemail = new UserService();
+    private static final UserService findByEmail = new UserService();
 
     public boolean login(String email, String password) {
-        String EmailFor = findbyemail.findByEmail(email, password);
-        if (EmailFor != null) {
-            return true;
-        }
-        return false;
+        String EmailFor = findByEmail.findByEmail(email, password);
+        return EmailFor != null;
     }
 }
