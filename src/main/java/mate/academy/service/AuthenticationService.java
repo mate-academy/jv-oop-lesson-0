70d2601f35dd72f7f18fb73bc6812a61e5837a93
muzykaVP <muzykavp.work@gmail.com>
2022-08-20 23:16:05 +0300

    implemented login method/implemented findByEmail method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -10,7 +10,12 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private static final UserService userService = new UserService();
     public boolean login(String email, String password) {
+
+        if (userService.findByEmail(email) != null) {
+            return userService.findByEmail(email).getPassword().equals(password);
+        }
         return false;
     }
 }
