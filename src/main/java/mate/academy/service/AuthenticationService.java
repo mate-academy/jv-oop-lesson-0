01f431ba24033d2dc1282a3c35a4e9075277bec9
GrishaSeniv <seniv.grisha@gmail.com>
2022-09-05 23:23:34 +0300

    implement login in AuthenticationService.java

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,9 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
+    UserService userService = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -11,6 +14,13 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        if(email == null || password == null){
+            return false;
+        }
+        User user = userService.findByEmail(email);
+        if(user != null && user.getPassword().equals(password)){
+            return true;
+        }
         return false;
     }
 }
