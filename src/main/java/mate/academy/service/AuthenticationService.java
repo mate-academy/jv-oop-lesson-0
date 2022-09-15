dc40278bbb44d745c5016bea9e65d65b30cd6fca
Volodymyr Mykychak <leadervlo.d@gmail.com>
2022-09-15 13:51:45 +0300

    solving a syntax problems

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,17 +5,18 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
         UserService userService = new UserService();
-        if(userService.findByEmail(email, password) == null){
+        if (userService.findByEmail(email, password) == null) {
             return false;
         }
-        return userService.findByEmail(email, password).getEmail().equals(email) &&
-                userService.findByEmail(email, password).getPassword().equals(password);
+        return userService.findByEmail(email, password).getEmail().equals(email)
+                && userService.findByEmail(email, password).getPassword().equals(password);
     }
 }
