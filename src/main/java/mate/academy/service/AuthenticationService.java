980e5d81c56fbdc6f51829461634833a81da81c7
Serhii <romanchuk552@gmail.com>
2022-09-15 16:10:37 +0300

    solved exercise "Working with services"

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,8 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -10,7 +13,15 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    public boolean login(String email, String password) {
-        return false;
+    public boolean login (String email, String password) {
+        boolean result = false;
+        for (User user : UserService.users) {
+            if (user.getEmail().equals(email)) {
+                if (user.getPassword().equals(password)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
     }
 }
