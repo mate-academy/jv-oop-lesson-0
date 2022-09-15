0cabcc63725d3a0ab24bd1afeae7fa91478329b8
Serhii <romanchuk552@gmail.com>
2022-09-15 16:17:04 +0300

    fixed problem with checkstyle

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,7 +1,6 @@
 package mate.academy.service;
 
 import mate.academy.model.User;
-import mate.academy.service.UserService;
 
 public class AuthenticationService {
     /**
@@ -13,7 +12,7 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    public boolean login (String email, String password) {
+    public boolean login(String email, String password) {
         boolean result = false;
         for (User user : UserService.users) {
             if (user.getEmail().equals(email)) {
