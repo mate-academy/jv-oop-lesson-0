78b8c2f6bb22af9e89c94dc15781622a75406a87
Nazarii Pertsak <pertsak98@gmail.com>
2022-09-16 12:54:37 +0300

    Fixed all style violations

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -12,7 +14,8 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         UserService service = new UserService();
-        if(service.findByEmail(email) != null && service.findByEmail(email).getPassword().equals(password)) {
+        User userService = service.findByEmail(email);
+        if (userService != null && userService.getPassword().equals(password)) {
             return true;
         }
         return false;
