4c24e275a33998c8d13cd67e0a992a9080fc7281
yehbreus <yeh.breus@gmail.com>
2022-09-20 17:38:13 +0300

    simplify code

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -14,10 +14,6 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         User user = UserService.findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
+        return user != null && user.getPassword().equals(password);
     }
 }
