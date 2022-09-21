8dfb1497f5b3a3d789fb92a79cf2321bae5705a5
Tymur Prokhorov <tymurprokhorov@gmail.com>
2022-09-21 16:46:33 +0300

    if-else simplified; comment deleted

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,10 +16,6 @@ public class AuthenticationService {
 
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        if (user != null && password.equals(user.getPassword())) {
-            return true;
-        } else {
-            return false;
-        }
+        return user != null && password.equals(user.getPassword());
     }
 }
