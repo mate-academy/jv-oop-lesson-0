a234a16de53849cc681421ebf0f691760548b3de
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-09-21 20:46:45 +0300

    deleted brackets and move userService to class AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,10 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
+    private UserService userService = new UserService();
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -11,7 +15,7 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return (UserService.findByEmail(email) != null && UserService.findByEmail(email)
-                .getPassword().equals(password));
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
