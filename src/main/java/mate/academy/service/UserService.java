aa98b26d9ca131544de01aad50bfc24c6f671095
thebitler <yurchyknikita@gmail.com>
2022-09-22 12:21:35 +0200

    Fixed code style problems

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,20 +3,24 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-	private static final User[] USERS_DATA = {
-			new User("bob@i.ua", "1234"),
-			new User("alice@i.ua", "1234")
-	};
+    private static final User[] USERS_DATA = {
+            new User("bob@i.ua", "1234"),
+            new User("alice@i.ua", "1234")
+    };
 
-	/**
-	 * Find user by email. All users are stored in <code>private static final User[] users</code>
-	 *
-	 * @param email - the input parameter
-	 * @return - user if his email is equal to passed email.
-	 * Return <code>null</code> if there is no suitable user
-	 */
-	public static User findByEmail(String email) {
-		for (User user : USERS_DATA) if (user.getEmail().equals(email)) return user;
-		return null;
-	}
+    /**
+     * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
+     * @param email - the input parameter
+     * @return - user if his email is equal to passed email.
+     * Return <code>null</code> if there is no suitable user
+     */
+    public static User findByEmail(String email) {
+        for (User user : USERS_DATA) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
