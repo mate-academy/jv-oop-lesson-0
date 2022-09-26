0f82ba934f3dd19ca637e8018f69041d56e400f2
Alex Kharchuk <alexander.kharchuk.vwcenter@gmail.com>
2022-09-26 12:07:48 +0300

    corrected sytle and changed for to foreach

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,22 +2,20 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
-import java.util.Arrays;
-
 public class AuthenticationService {
-     /**
-     * Imagine that some user wants to log in to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+    /**
+    * Imagine that some user wants to log in to your site.
+    * You should check if user credentials (login and password) are valid or not.
+    * All users are stored in <code>UserService</code> class.
+    * @param email - user's email
+    * @param password - user's password
+    * @return true if user by email exists and passed password is equal to user's password.
+    * Return false in any other cases.
+    */
 
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User currentUser = userService.findByEmail(email);
-        return userService.findByEmail(email) != null && currentUser.getPassword().equals(password) ? true : false;
+        return userService.findByEmail(email) != null && currentUser.getPassword().equals(password);
     }
 }
