749053dca21e42bb61d48a6b490b6f672fc8b0a5
Uka0001 <ukraine0001@gmail.com>
2022-11-01 22:28:52 +0200

    implemented findByEmail method and login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,8 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+import static mate.academy.service.UserService.users;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +14,11 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        for (User each: users) {
+            if (each.getEmail().equals(email) & each.getPassword().equals(password)){
+                return true;
+            }
+        }
         return false;
     }
 }
