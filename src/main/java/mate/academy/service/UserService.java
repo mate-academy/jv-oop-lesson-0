2e971a92e22275065c2c583f9031155ab1fe6fa8
Yurii Dikalo <dikalotel@gmail.com>
2022-11-08 14:53:03 +0200

    Implemented findByEmail and login methods

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,18 +3,25 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        User foundUser = null;
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                foundUser = user;
+            }
+        }
+        return foundUser;
     }
 }
