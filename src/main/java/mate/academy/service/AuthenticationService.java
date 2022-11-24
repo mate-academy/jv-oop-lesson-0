8123dd292602f42aa6dd40b89484620392306fa0
Oleg <96373763+Olegator191@users.noreply.github.com>
2022-11-24 10:27:06 +0200

    Added solution1

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,8 @@
 package mate.academy.service;
 
 public class AuthenticationService {
+
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
