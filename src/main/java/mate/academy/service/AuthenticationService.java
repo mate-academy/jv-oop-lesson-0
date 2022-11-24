f585a0d621f4b756f296b0033f6f889184da4ab3
Mykola Radzivon <kolian201897@gmail.com>
2022-11-25 00:26:43 +0200

    fixed code style

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,12 +2,10 @@ package mate.academy.service;
 
 public class AuthenticationService {
 
-    private UserService user = new UserService();
+    private final UserService user = new UserService();
 
     public boolean login(String email, String password) {
-        if ((user.findByEmail(email) != null && user.findByEmail(email).getPassword() == password)) {
-            return true;
-        }
-        return false;
+        return user.findByEmail(email) != null
+                && user.findByEmail(email).getPassword().equals(password);
     }
 }
