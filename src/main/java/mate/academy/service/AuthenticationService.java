ce1b6b6b201e059741cbb268139a020d5ff88453
Mykola Radzivon <kolian201897@gmail.com>
2022-11-25 11:51:16 +0200

    added new variable

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,11 +1,13 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
 
     private final UserService user = new UserService();
 
     public boolean login(String email, String password) {
-        return user.findByEmail(email) != null
-                && user.findByEmail(email).getPassword().equals(password);
+        User info = user.findByEmail(email);
+        return info != null && info.getPassword().equals(password);
     }
 }
