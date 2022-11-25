3b01f3c38ee500cc2cfde008d424a399b997693b
Mykola Radzivon <kolian201897@gmail.com>
2022-11-25 12:23:57 +0200

    Change name info - user and user -users

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -4,10 +4,10 @@ import mate.academy.model.User;
 
 public class AuthenticationService {
 
-    private final UserService user = new UserService();
+    private final UserService users = new UserService();
 
     public boolean login(String email, String password) {
-        User info = user.findByEmail(email);
-        return info != null && info.getPassword().equals(password);
+        User user = users.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
