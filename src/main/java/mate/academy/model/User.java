59270eda602f472ce25f6043bbfc92c713a22166
Olexiy <korm2909@gmail.com>
2022-12-04 17:56:03 +0200

    hw-2

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,9 +1,7 @@
 package mate.academy.model;
-
 public class User {
     private String email;
     private String password;
-
     public User(String email, String password) {
         this.email = email;
         this.password = password;
