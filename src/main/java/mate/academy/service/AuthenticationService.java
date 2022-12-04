7139469c6734a1ec041d04d30c0a2b7830ae010e
Dastya <daniillipenec@gmail.com>
2022-12-04 18:25:08 +0200

    added solution, moved line to the class level

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    UserService service = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -12,9 +13,7 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-
     public boolean login(String email, String password) {
-        UserService service = new UserService();
         User user = service.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,19 +3,22 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
+    long abc = 1000L;
+    float bca = 1000f;
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        for (User user:users) {
+        for (User user : users) {
             if (user.getEmail().equals(email)) {
                 return user;
             }
