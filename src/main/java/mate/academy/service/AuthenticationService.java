9e7d9733f124f68c4cf0e7a6d2537bedd9c1234c
Olexiy <korm2909@gmail.com>
2022-12-06 21:30:04 +0200

    look fine

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,14 +5,16 @@ import mate.academy.model.User;
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
-     @@ -10,7 +12,10 @@ public class AuthenticationService {
-      * @return true if user by email exists and passed password is equal to user's password.
+     * You should check if user credentials (login and password) are valid or not.
+     * All users are stored in <code>UserService</code> class.
+     * @param email - user's email
+     * @param password - user's password
+     * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    private UserService userService = new UserService();
     public boolean login(String email, String password) {
-        return false;
-        User user = userService.findByEmail(email);
+        UserService service = new UserService();
+        User user = service.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,14 +1,15 @@
+package mate.academy.service;
+
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[]{
+    private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
-     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
@@ -22,3 +23,4 @@ public class UserService {
         return null;
     }
 }
+
