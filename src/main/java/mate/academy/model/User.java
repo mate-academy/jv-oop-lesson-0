3aab36795eccf838a69798c28a71ee2c5538c28c
Olexiy <korm2909@gmail.com>
2022-12-06 21:35:07 +0200

    looks fine

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,7 +1,9 @@
 package mate.academy.model;
+
 public class User {
     private String email;
     private String password;
+
     public User(String email, String password) {
         this.email = email;
         this.password = password;
