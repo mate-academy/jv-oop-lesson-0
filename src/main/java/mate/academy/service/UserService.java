9202fedbbc48befe080e0a2bbe0a7f73f255db34
Denys Dominskyi <dominskyj@gmail.com>
2023-01-08 16:17:52 +0200

    initial commit

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,18 +3,21 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
-    /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
-     */
+    public static User[] getUsers() {
+        return users;
+    }
+
     public User findByEmail(String email) {
+        for (int i = 0; i < getUsers().length; i++) {
+            if (getUsers()[i].getEmail().equals(email)) {
+                return getUsers()[i];
+            }
+        }
         return null;
     }
 }
