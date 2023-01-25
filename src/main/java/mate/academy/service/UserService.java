8cbe8932b1481baaff45a1bf5073a02812ef09bb
Artem Ponomarenko <artemponomar98@gmail.com>
2023-01-25 16:36:25 +0200

    implemented methods UserService.findByEmail and AuthenticationService.login

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -2,6 +2,8 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
+import java.util.Arrays;
+
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
@@ -15,6 +17,6 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        return Arrays.stream(users).filter(u -> u.getEmail().equals(email)).findFirst().orElse(null);
     }
 }
