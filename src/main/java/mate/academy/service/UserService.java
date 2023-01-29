27c7e89b4ee4849cc29dd498ba6b7a5b78053e96
Maksym <maxim.yashyn@gmail.com>
2023-01-29 20:54:15 +0200

    added a new user

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -5,7 +5,8 @@ import mate.academy.model.User;
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
-            new User("alice@i.ua", "1234")
+            new User("alice@i.ua", "1234"),
+            new User("john@i.ua", "1234")
     };
 
     /**
