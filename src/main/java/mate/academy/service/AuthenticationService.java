462974fbfffdf6a3820058717684411fdacd42ce
Daunito <gatne2004@gmail.com>
2023-01-30 13:49:13 +0100

    not 1

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,8 +13,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-    UserService userService = new UserService();
-	User user = userService.findByEmail(email);
-    return user != null && user.getPassword().equals(password);
+      UserService userService = new UserService();
+	  User user = userService.findByEmail(email);
+      return user != null && user.getPassword().equals(password);
     }
 }
