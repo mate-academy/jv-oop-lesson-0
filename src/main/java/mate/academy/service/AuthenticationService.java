ebc2fb95ace77cd15bf33cab5f7e7ccc85870610
potrap <potrapmax@gmail.com>
2023-01-31 12:25:15 +0200

    method was simplified

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,11 +1,11 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
 
     public boolean login(String email, String password) {
-        if (new UserService().findByEmail(email) == null) {
-            return false;
-        }
-        return new UserService().findByEmail(email).getPassword().equals(password);
+        User user = new UserService().findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
