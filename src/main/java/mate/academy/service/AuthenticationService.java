fc5f668cc4b9a3d45104c83c3a3eb9396c13aa53
ivan <kiohi2000@gmail.com>
2023-02-08 11:09:50 +0200

    bugs fixed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -14,10 +14,7 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         UserService userService = new UserService();
-        User userX = userService.findByEmail(email);
-        if (userX != null && userX.getPassword().equals(password)){
-            return true;
-        }
-        return false;
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
