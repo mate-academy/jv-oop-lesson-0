97ff1502358f704c93bdd31955fe3b9b7711d390
Stas Zlishchev <zlishchev@gmail.com>
2023-03-26 16:24:37 +0300

    fixed minor mistakes

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,7 +15,7 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        if(user == null) {
+        if (user == null) {
             return false;
         }
         return user.getPassword().equals(password);
