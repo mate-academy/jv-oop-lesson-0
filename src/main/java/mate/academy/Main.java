2276b2836726acb8d1c61ba92bcf917d1e06242e
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-30 15:23:54 +0300

    implement login and findByEmail methods

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static final AuthenticationService authenticationService = new AuthenticationService();
 
     public static void main(String[] args) {
-        test("bob@i.ua", "12354", true);
+        test("bob@i.ua", "1234", true);
         test("alice@i.ua", "1234", true);
         test("john@i.ua", "1234", false);
         test("bob@i.ua", "qwerty", false);
