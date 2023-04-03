92bc2f377187807777ded4c5a1432ffe7d78993e
Arthur Dvinskykh <dv7art@gmail.com>
2023-04-03 14:51:35 +0300

    Implemented_Task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -10,7 +10,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    UserService userService = new UserService();
     public boolean login(String email, String password) {
-        return false;
+
+       return userService.findByEmail(email,password) != null ? true: false;
+
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -13,8 +13,17 @@ public class UserService {
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
+
+     * public User findByEmail(String email) {
+        return null;
+    }
      */
-    public User findByEmail(String email) {
+    public User findByEmail(String email, String password) {
+        for(User user: users) {
+            if(user.getEmail().equals(email) && user.getPassword().equals(password)){
+                return  user;
+            }
+        }
         return null;
     }
 }
