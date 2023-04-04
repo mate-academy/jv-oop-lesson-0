bb502a13b71bb0c096c896b42c2d8ddf13035c88
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-04-04 17:10:02 +0300

    add setters for class User

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -10,10 +10,19 @@ public class User {
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public String getPassword() {
+
         return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
