4386495146808a48141a329e67cd1d4b424c5587
AnastasiiaVariuchuk <stacyvariuchuk@gmail.com>
2023-04-05 18:47:55 +0300

    fix problem2

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
