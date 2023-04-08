3d2ae931d77f7be03c560fd63ba6938ff9f8e082
AnastasiiaVariuchuk <stacyvariuchuk@gmail.com>
2023-04-08 18:03:06 +0300

    add BUILD SUCCESS

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,6 +7,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        return user!= null && password.equals(user.getPassword());
+        return user != null && password.equals(user.getPassword());
     }
 }
