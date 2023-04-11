bf77590c0f99ddfa16953974161153a1dd0553c4
Yaroslav Khmaruk <127929863+ykhmaruk@users.noreply.github.com>
2023-04-11 17:17:37 +0300

    Update UserService.java

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -13,9 +13,9 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+    public User findByEmail(String email, String password) {
         for (User user : users) {
-            if (user.getEmail().equals(email)) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
                 return user;
             }
         }
