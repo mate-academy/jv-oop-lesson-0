c53eabf4d6ec5849ad3ca252b2ac61860bef8930
Vitalii <razzhivinner@gmail.com>
2023-04-12 23:19:24 +0300

    Implemented two methods and verify my solution.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        User test = UserService.findByEmail(email);
+        if (test != null) {
+            if (password.equals(test.getPassword())) {
+                return true;
+            }
+        }
         return false;
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -8,13 +8,20 @@ public class UserService {
             new User("alice@i.ua", "1234")
     };
 
+
+
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+    public static User findByEmail(String email) {
+        if (email.equals(users[0].getEmail())) {
+            return users[0];
+        } else if (email.equals(users[1].getEmail())) {
+            return users[1];
+        }
         return null;
     }
 }
