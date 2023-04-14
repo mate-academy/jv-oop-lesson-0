07cce46010225f032ce0a91f28be9ed9d3e4cb3e
Arina Polna <polna.arina@gmail.com>
2023-04-14 06:15:13 +0300

    implemented login and findByEmail methods

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -10,7 +11,12 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
-        return false;
+        UserService UserServiceResult = new UserService();
+        User checkUser = UserServiceResult.findByEmail(email);
+        return checkUser != null && password.equals(checkUser.getPassword());
+
     }
+
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,18 +3,27 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
+     * public User findByEmail(String email) {
+     * return null;
+     * }
      */
     public User findByEmail(String email) {
-        return null;
+        for (User checkUser : users)
+            if (email.equals(checkUser.getEmail())) {
+                return checkUser;
+            }
+            return null;
     }
 }
+
