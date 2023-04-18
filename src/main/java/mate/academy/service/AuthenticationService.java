1b873453603fcef59493e95c8e6941ed82b316ef
Vladyslav <vladkrivec3@gmail.com>
2023-04-18 11:02:17 +0300

    code simplify

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -22,12 +22,8 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        if (user == null) {
-            return false;
-        }
-        return user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
