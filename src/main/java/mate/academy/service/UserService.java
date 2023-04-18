c1d396cc102288769cb5c6f0b16e2165a1683fd3
mariia_mytrofanova <130451095+mary-mi@users.noreply.github.com>
2023-04-18 14:57:18 +0300

    Update UserService.java

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -7,7 +7,6 @@ public class UserService {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
-
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      * @param email - the input parameter
@@ -15,7 +14,8 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        for(User user: users) {
+        UserService userService = new UserService();
+        for(User user: userService.getUsers()) {
             if(user.getEmail().equals(email)) {
                 return user;
         }
