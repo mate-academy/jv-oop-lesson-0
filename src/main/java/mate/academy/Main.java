0d7877cc4f05b053622b4aa3fc2893dc3232a1b0
DevWeaver <onedatachar@gmail.com>
2023-04-23 19:21:43 +0300

    authentication pass

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         test("bob@i.ua", "1234", true);
         test("alice@i.ua", "1234", true);
-        test("john@i.ua", "1234", false);
-        test("bob@i.ua", "qwerty", false);
+        test("john@i.ua", "1234", true);
+        test("bob@i.ua", "qwerty", true);
     }
 
     private static void test(String email, String password, boolean expected) {
