4a5f795eb542843608a20623ec53955d256159d5
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-06-17 11:23:00 +0300

    corrected codestyle and optimized login()

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,16 +3,13 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-
     private UserService userService = new UserService();
 
     public boolean login(String email, String password) {
         User foundUser = userService.findByEmail(email);
-
         if (foundUser == null) {
             return false;
         }
-        return foundUser.getEmail().equals(email)
-                && foundUser.getPassword().equals(password);
+        return foundUser.getPassword().equals(password);
     }
 }
