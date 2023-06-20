5053088f9efb13f5984c8c80fa08aca3ab551a8b
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-06-20 16:44:11 +0300

    'login' and 'findByEmail' methods are implemented

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -14,7 +14,10 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+    public static User findByEmail(String email) {
+        for (User user : users) {
+            if(user.getEmail().equals(email)) return user;
+        }
         return null;
     }
 }
