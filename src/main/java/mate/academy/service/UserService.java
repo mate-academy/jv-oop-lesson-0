a91c79682c0d328ff665137497d3363d6bf615c9
alezalez8 <5446943@ukr.net>
2023-06-21 17:46:57 +0300

    hw-0 is done

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -16,9 +16,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        for (int i = 0; i < users.length; i++) {
-            if (email.equals(users[i].getEmail())) {
-                return users[i];
+
+        for (User user : users
+        ) {
+            if (email.equals(user.getEmail())) {
+                return user;
             }
         }
         return null;
