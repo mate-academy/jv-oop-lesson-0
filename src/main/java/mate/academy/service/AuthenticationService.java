87a2eca9c155a3e310db4ff30cbd6397b942ea28
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-07-26 21:36:40 +0300

    renamed variable

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,8 +13,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService us = new UserService();
-        User user = us.findByEmail(email);
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
