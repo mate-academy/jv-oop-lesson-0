3bf20982c7fb80c67e7cd6ca581dafd32dcb4ad6
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-07-27 14:08:13 +0300

    fixed login method and delete getUsers method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,12 +13,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        User[] users = UserService.getUsers();
-        for (User user : users) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
