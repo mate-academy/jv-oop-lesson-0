1039b8d68cdb33a8417431f13e4083a08a3472f2
AdamMudrak <mudrets2000@gmail.com>
2023-09-23 13:40:06 +0300

    Improve codestyle

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -7,6 +7,7 @@ public class UserService {
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
