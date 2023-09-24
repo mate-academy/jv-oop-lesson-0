5fefb8ed2f4185472deccb5979c590e95da20f1a
Hlib Kulikov <forzen123212@proton.me>
2023-09-24 19:47:19 +0300

    Added verification methods

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    static private final User[] users = new User[] {
+    static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
@@ -16,7 +16,9 @@ public class UserService {
      */
     public static User findByEmail(String email) {
         for (User user: users) {
-            if (user.getEmail().equals(email)) return user;
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
         }
         return null;
     }
