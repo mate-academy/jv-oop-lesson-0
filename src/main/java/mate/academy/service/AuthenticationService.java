9a5f18b3b8671d45d65ed527476a83456068aa99
Vadym Hurnik <vad.com.z@gmail.com>
2023-09-24 16:46:46 +0300

    fixed comparison between strings and simplified return

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,6 +15,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        return user != null && user.getPassword() == password ? true : false;
+        return user != null && user.getPassword().equals(password);
     }
 }
