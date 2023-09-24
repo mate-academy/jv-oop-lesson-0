86fa5689a164f3b5c1f34db22fd40ff6be59d60b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-24 12:17:09 +0300

    added changes to the class AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -17,8 +17,9 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-            return user != null && user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
