064443039d47c000fbf9121ad7b784d947e289ec
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2023-09-25 11:17:30 +0300

    Improved login method in AuthenticationService.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,18 +7,17 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        User user = userService.findByEmail(email);
+        User user = new UserService().findByEmail(email);
         if (user != null) {
-            return user.getEmail().equals(email) && user.getPassword().equals(password);
-        } else {
-            return false;
+            return user.getPassword().equals(password);
         }
+        return false;
     }
 }
