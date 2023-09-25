bd5c5865cf2f9ccf040677e7297fcf171422d5ad
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-09-25 11:22:17 +0300

    fixed the mistakes

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,12 +3,21 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    /**
+     * Imagine that some user wants to login to your site.
+     * You should check if user credentials (login and password) are valid or not.
+     * All users are stored in <code>UserService</code> class.
+     * @param email - user's email
+     * @param password - user's password
+     * @return true if user by email exists and passed password is equal to user's password.
+     * Return false in any other cases.
+     */
+    private UserService userService = new UserService();
+    private User[] users = userService.getUsers();
 
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        User[] getUsers = userService.getUsers();
-        for (User user : getUsers) {
-            if (user.getEmail() == email && user.getPassword() == password) {
+        for (User user : users) {
+            if (email.equals(user.getEmail()) && password.equals(user.getPassword())) {
                 return true;
             }
         }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[]{
+    private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
@@ -14,7 +14,6 @@ public class UserService {
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
-     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
