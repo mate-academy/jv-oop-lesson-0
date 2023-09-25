ac69aec5ee4c819ef9ea6d25cc6d21b38afdbd26
petro feniak <petro.fenyak@gmail.com>
2023-09-25 18:55:12 +0300

    realised method findByEmail in class UserService

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,18 +3,26 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User user :
+                users) {
+            if (email.equals(user.getEmail())) {
+                return user;
+            }
+        }
         return null;
     }
 }
+
