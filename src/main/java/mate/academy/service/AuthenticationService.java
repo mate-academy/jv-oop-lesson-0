3e6bc72672821e454ab2f94ef7c8d46a42211f56
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-09-25 12:40:11 +0300

    Replaced loop from for to foreach in findByEmail method in UserService Deleted modifier "static" in findByEmail in UserService Renamed param "password" to "inputtedPassword" in login method in AuthenticationService Changed implementation of login method in AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,21 +1,19 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
-     * Imagine that some user wants to login to your site.
+     * Imagine that some user wants to log in to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
      * @param email - user's email
-     * @param password - user's password
+     * @param inputtedPassword - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    public boolean login(String email, String password) {
-        if (UserService.findByEmail(email) != null) {
-            if (UserService.findByEmail(email).getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
+    public boolean login(String email, String inputtedPassword) {
+        User userByEmail = new UserService().findByEmail(email);
+        return userByEmail.getPassword().equals(inputtedPassword);
     }
 }
