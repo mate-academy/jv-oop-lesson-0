e3626bb10b9995d586536d058986dfcecf4c53e5
Myself <wetball74@gmail.com>
2023-09-25 15:25:29 +0300

    User constructor deleted

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -4,10 +4,6 @@ public class User {
     private String email;
     private String password;
 
-    public User(){
-
-    }
-
     public User(String email, String password) {
         this.email = email;
         this.password = password;
