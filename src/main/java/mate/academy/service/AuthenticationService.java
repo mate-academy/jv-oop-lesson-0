abd68ba1599fed6e3a48e794cf0943bd6eeda329
Nataliia <gzodchie@gmail.com>
2023-09-25 11:49:16 +0300

    change userService status on class memeber

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,8 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private UserService userService = new UserService();
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -13,7 +15,6 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
         User user = userService.findByEmail(email);
 
         if (user != null && user.getPassword().equals(password)) {
