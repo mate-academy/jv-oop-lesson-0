a13b7b4ac42ed559232f08cdfd694f79651864fb
unknown <liubchenkalexander@gmail.com>
2023-09-26 16:06:44 +0300

    if in login simplified, static from UserService delited, found renamed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-    private static final UserService userService = new UserService();
+    private final UserService userService = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -15,10 +15,7 @@ public class AuthenticationService {
      */
 
     public boolean login(String email, String password) {
-        User found = userService.findByEmail(email);
-        if (found != null) {
-            return found.getPassword().equals(password);
-        }
-        return false;
+        User userFound = userService.findByEmail(email);
+        return userFound != null && userFound.getPassword().equals(password);
     }
 }
