23fc339acc9aa50a05173ba90a4a4aa158a54f8b
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-09-26 12:53:40 +0300

    Refactor login method: Remove redundant email check

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,9 +16,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email, password);
-        if (user == null) {
-            return false;
-        }
-        return user.getEmail().equals(email) && user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
