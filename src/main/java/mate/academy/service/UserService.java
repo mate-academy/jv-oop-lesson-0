08de3f6993fc8692e73f3238d8117d9784f10729
Oleksii Borysenko <343502597s@gmail.com>
2023-09-26 09:49:10 +0200

    mvn clean ok

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User user : users) {
+            if (email != null && email.equals(user.getEmail())) {
+                return user;
+            }
+        }
         return null;
     }
 }
