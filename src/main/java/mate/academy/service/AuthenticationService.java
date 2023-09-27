42eb8e95a2833f42e142eb31843b1e9573fcf8f8
Roman K <romazan07sq@gmail.com>
2023-09-27 21:06:49 +0300

    simplify AuthenticationService class

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,6 +7,7 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
+     *
      * @param email - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
@@ -16,12 +17,7 @@ public class AuthenticationService {
     private UserService userService = new UserService();
 
     public boolean login(String email, String password) {
-        User user = new User(email, password);
-        if (userService.findByEmail(email) != null) {
-            if (userService.findByEmail(email).getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
