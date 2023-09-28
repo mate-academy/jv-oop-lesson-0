18f91116f6088d798e08f35722d0b5962f73c137
Vlad Andriichuk <vlad150096@gmail.com>
2023-09-28 20:15:24 +0200

    Realized methods findByEmail() in UserService class and login() in AuthenticationService class.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        UserService userService = new UserService();
+        return userService.findByEmail(email) != null
+                && userService.findByEmail(email).getPassword().equals(password);
     }
 }
