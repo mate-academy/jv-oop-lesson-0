24a6fac6bd3bf9b36e4e0a820233c51f16b6ce8e
Eugene Korniuk <korniuk77@gmail.com>
2023-10-06 19:32:01 +0300

    add login method implementation, add findByEmail method implementation

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        UserService passService = new UserService();
+        return passService.findByEmail(email) != null && passService.findByEmail(email)
+                .getPassword().equals(password);
     }
 }
