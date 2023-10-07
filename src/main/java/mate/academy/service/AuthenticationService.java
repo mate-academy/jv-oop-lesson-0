612ce41ea649399c1023760ff62b5d68e53112cd
Alexander Karpenko <karalexander@gmail.com>
2023-10-08 00:32:31 +0300

    the changes to the methods were made

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,9 +15,6 @@ public class AuthenticationService {
     
     public boolean login(String email, String password) {
         User user = new UserService().findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        return user != null && user.getPassword().equals(password);
     }
 }
