6a0e41c2a9ff846c2daf6146fa37bf93fda365c9
Nazar <oliya37@gmail.com>
2023-10-07 14:53:11 +0300

    made simplify

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,14 +3,12 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-    public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        User userLogin = userService.findByEmail(email);
 
-        if (userLogin != null && password.equals(userLogin.getPassword())) {
-            return true;
-        } else {
-            return false;
-        }
+    private UserService userService = new UserService();
+
+    public boolean login(String email, String password) {
+
+        User userLogin = userService.findByEmail(email);
+        return userLogin != null && password.equals(userLogin.getPassword());
     }
 }
