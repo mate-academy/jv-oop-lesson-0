b2ccd36d18debfd528707eb35174bc30974d4fdc
Alexander Karpenko <karalexander@gmail.com>
2023-10-11 23:47:34 +0300

    userService was declared as the lass-level field

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,9 +12,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    
+    private final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        User user = new UserService().findByEmail(email);
+        User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
