eae2a0364ce93c9d57971851c03da4b34befd739
Anton Babych <antonbabi13@gmail.com>
2023-10-13 22:12:25 +0300

    rename UserService object from uS to userService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,17 +7,15 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     *
      * @param email - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    UserService uS = new UserService();
 
     public boolean login(String email, String password) {
-        UserService uS = new UserService();
-        User user = uS.findByEmail(email);
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
         if (user != null && user.getPassword().equals(password)) {
             return true;
         }
