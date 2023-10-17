dac4a3e55de114d7edc1eb8739cab6151ed02a8d
Taras Kushnir <taras.kushnir.tk@gmail.com>
2023-10-17 12:49:56 +0200

    correct class AuthenticationService and UserService

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,20 +1,30 @@
 package mate.academy.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    protected static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
-        return null;
+    public User[] findByEmail(String email) {
+        List<User> userEmail = new ArrayList<>();
+        for (User userFind : users) {
+            if (userFind.getEmail().equals(email)) {
+                userEmail.add(userFind);
+            }
+        }
+        return userEmail.toArray(new User[0]);
     }
 }
+
