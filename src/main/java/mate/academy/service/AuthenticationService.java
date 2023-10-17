1b982ede3f943722cd7dc86399b708472bd67629
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-10-17 21:22:03 +0200

    if else structure in the boolean method "login" has been simplified

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,9 +16,6 @@ public class AuthenticationService {
 
     public boolean login(String email, String password) {
         User userByEmail = userService.findByEmail(email);
-        if (userByEmail != null && userByEmail.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        return userByEmail != null && userByEmail.getPassword().equals(password);
     }
 }
