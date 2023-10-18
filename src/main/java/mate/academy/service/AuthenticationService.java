80abda2f07dff4b656720957aed9ba93d6763a3a
Sasha <sasha.panchyshun@gmail.com>
2023-10-18 13:37:37 +0300

    fix login

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,23 +1,22 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
 
     public boolean login(String email, String password) {
-        for (int i = 0; i < UserService.getUsers().length; i++) {
-            if (UserService.getUsers()[i].getEmail().equals(email)
-                    && UserService.getUsers()[i].getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
+        UserService userService = new UserService();
+        User byEmail = userService.findByEmail(email);
+        return byEmail != null && byEmail.getPassword().equals(password);
     }
 }
