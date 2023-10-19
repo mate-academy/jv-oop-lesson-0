c7f9a044392236d8c31199ddf443c20a38a7d1cf
Andrii Khomenko <andpey.homenko@gmail.com>
2023-10-19 16:22:46 +0300

    removed the loop in the login() method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,11 +12,12 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        for (User user: UserService.users) {
-            if (email.equals(user.getEmail()) && password.equals(user.getPassword())) {
-                return true;
-            }
+        User user = userService.findByEmail(email);
+        if (user != null && password.equals(user.getPassword())) {
+            return true;
         }
         return false;
     }
