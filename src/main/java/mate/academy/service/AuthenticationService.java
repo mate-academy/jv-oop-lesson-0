5a601665262271da66ba2472bb4b9485b739eac0
Roman Globa <globaroman@gmail.com>
2023-10-23 22:13:05 +0300

    Added solution to method UserService and AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,8 @@
 package mate.academy.service;
 
+import java.util.Optional;
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +14,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService service = new UserService();
+        Optional<User> user = Optional.ofNullable(service.findByEmail(email));
+        if (user.isPresent()) {
+            return user.get().getPassword().equals(password);
+
+        }
         return false;
     }
 }
