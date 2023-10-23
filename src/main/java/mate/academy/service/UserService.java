8260c9f24a751263e3e1ac108d947d2a8c9c2b38
Taras Kushnir <taras.kushnir.tk@gmail.com>
2023-10-23 23:24:42 +0200

    change protect to privete and fixed UserService

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,11 +1,9 @@
 package mate.academy.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import mate.academy.model.User;
 
 public class UserService {
-    protected static final User[] users = new User[]{
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
@@ -17,14 +15,13 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User[] findByEmail(String email) {
-        List<User> userEmail = new ArrayList<>();
-        for (User userFind : users) {
-            if (userFind.getEmail().equals(email)) {
-                userEmail.add(userFind);
+    public User findByEmail(String email) {
+        for (User user : users) {
+            if (email.equals(user.getEmail())) {
+                return user;
             }
         }
-        return userEmail.toArray(new User[0]);
+        return null;
     }
 }
 
