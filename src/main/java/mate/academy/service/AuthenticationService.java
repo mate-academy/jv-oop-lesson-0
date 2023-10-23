0bc9fe0aee9915035fa0732d4e976f7d518b200e
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2023-10-23 21:41:01 +0300

    work branch was created, findByEmail method was implemented

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,10 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService userService = new UserService();
+        if (email.equals(userService.findByEmail(email)) && password.equals(userService.findByEmail(password))) {
+            return true;
+        }
         return false;
     }
 }
