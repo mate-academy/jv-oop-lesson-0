35b8450c3adedddf8cb346b8cc2fd29425cf4d54
Roman Globa <globaroman@gmail.com>
2023-10-25 13:50:01 +0300

    Fixed errors indicated by the mentor: Replaced ref to UserService as a class field; Removed 'Optional'.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,5 @@
 package mate.academy.service;
 
-import java.util.Optional;
 import mate.academy.model.User;
 
 public class AuthenticationService {
@@ -8,16 +7,19 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
+     *
      * @param email - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private final UserService service = new UserService();
+
     public boolean login(String email, String password) {
-        UserService service = new UserService();
-        Optional<User> user = Optional.ofNullable(service.findByEmail(email));
-        if (user.isPresent()) {
-            return user.get().getPassword().equals(password);
+        User user = service.findByEmail(email);
+        if (user != null) {
+
+            return user.getPassword().equals(password);
 
         }
         return false;
