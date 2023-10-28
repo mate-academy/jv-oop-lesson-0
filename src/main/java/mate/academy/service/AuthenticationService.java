a8aef0f3ed4423870b5ea5f08d9bf39fa919d1bd
Nazar <nazarmos13@gmail.com>
2023-10-28 14:26:16 +0300

    did my homework

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,13 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        User[] users = UserService.getUsers();
+        for (User user : users) {
+            if (email.equals(user.getEmail())
+                    && password.equals(user.getPassword())) {
+                return true;
+            }
+        }
         return false;
     }
 }
