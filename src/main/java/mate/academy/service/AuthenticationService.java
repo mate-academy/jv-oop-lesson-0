d26bc94402b87ab5080278d783dd9d985ae51c9b
Yevhen Nedashkivskyi <kesh040593@ukr.net>
2023-10-29 17:32:11 +0200

    Fix a login method in AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     private UserService userService;
 
@@ -7,15 +9,14 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        boolean isCurrentUser = userService.findByEmail(email) != null
-                && userService.findByEmail(email).getPassword().equals(password);
-        return isCurrentUser;
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 
 }
