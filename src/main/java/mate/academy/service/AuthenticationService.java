d017bc7e5aa5db3298e0de1e718339e62a00a2d6
Serhii Makarchuk <sergiymakarchuk1507@gmail.com>
2023-10-29 15:34:35 +0200

    fixed UserService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,8 +13,13 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
     */
+    private UserService userService;
+
+    public AuthenticationService() {
+        this.userService = new UserService();
+    }
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
         User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
