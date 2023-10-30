6ef44bd53e14797ec9a8574450b09911edfbc86b
Serhii Kopervas <sergkopervas@gmail.com>
2023-10-30 20:52:48 +0100

    Implementation of user authentication

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,6 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        return new UserService().findByEmail(email).getPassword().equals(password);
     }
 }
