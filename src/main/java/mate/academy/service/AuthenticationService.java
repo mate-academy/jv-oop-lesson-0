f7cd5f4d2b84600da370211c88b423b5b12b4b76
Andriy <gan.andriy@gmail.com>
2023-11-02 15:46:59 +0100

    optimizing code

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,10 +16,6 @@ public class AuthenticationService {
 
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        if (user == null) {
-            return false;
-        } else {
-            return user.getEmail().equals(email) && user.getPassword().equals(password);
-        }
+        return user != null && user.getPassword().equals(password);
     }
 }
