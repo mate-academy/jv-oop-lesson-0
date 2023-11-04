a843080321bb1b147f4873edf9a4487714d4c9ff
Orest 40th <student2528@bmet.org.ua>
2023-11-04 15:50:37 +0200

    findByEmail method added to UserService.java login method added to  AuthenticationService.java

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,7 +2,7 @@ package mate.academy.service;
 
 public class AuthenticationService {
     /**
-     * Imagine that some user wants to login to your site.
+     * Imagine that some user wants to log in to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
      * @param email - user's email
@@ -11,6 +11,10 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        if (UserService.findByEmail(email) != (null)
+                && UserService.findByEmail(email).getPassword().equals(password)) {
+            return true;
+        }
         return false;
     }
 }
