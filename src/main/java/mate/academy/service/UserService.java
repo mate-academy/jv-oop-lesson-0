523a7b39543a5189f6ae53b608238ce784d54764
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2023-11-05 18:28:13 +0200

    implemented a login method in AuthenticationService and findByEmail method in UserService

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,18 +3,30 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        User requiredUser = null;
+        for (User user : users) {
+            if (email.equals(user.getEmail())) {
+                requiredUser = user;
+            }
+        }
+
+        return requiredUser;
+    }
+
+    public static User[] getUsers() {
+        return users;
     }
 }
