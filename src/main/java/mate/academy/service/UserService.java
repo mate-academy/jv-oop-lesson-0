646697d31c05b698a306eba76480774c678e0d78
Igor Hunder <gunderigorv@gmail.com>
2023-11-11 17:39:52 +0200

    reworked the method of checking user data

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -7,7 +7,6 @@ public class UserService {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
-
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      *
@@ -15,6 +14,7 @@ public class UserService {
      *              * @return - user if his email is equal to passed email.
      *              * Return <code>null</code> if there is no suitable user
      */
+
     public User findByEmail(String email) {
         for (User user : users) {
             if (user.getEmail().equals(email)) {
