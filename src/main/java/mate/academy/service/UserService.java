0f2875e595f8db327c6effb53725b51caed3e123
Yaroslav Berezhniak <yaroslavix100@gmail.com>
2023-11-13 17:11:22 +0200

    task completed

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -14,7 +14,7 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public static User findByEmail(String email) {;
+    public static User findByEmail(String email) {
         for (User user : users) {
             if (email.equals(user.getEmail())) {
                 return user;
