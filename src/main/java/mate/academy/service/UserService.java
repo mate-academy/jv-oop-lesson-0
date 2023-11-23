1f7f51f4c7c3f071d6a13796fb808123355ad547
VolandevlodD <vladradko2000@gmail.com>
2023-11-23 16:48:41 +0200

    login method in AuthenticationService and findByEmail method in UserService was implemented

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User temp : users) {
+            if (temp.getEmail().equals(email)) {
+                return temp;
+            }
+        }
         return null;
     }
 }
