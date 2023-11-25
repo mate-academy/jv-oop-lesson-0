506085275038d85443eb637079fbe4fa8c74ed20
John <pandastars535@gmail.com>
2023-11-25 14:07:41 -0500

    removed extends from authenticationService checked mvn clean package

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,7 +2,7 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
-public class AuthenticationService extends UserService {
+public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
