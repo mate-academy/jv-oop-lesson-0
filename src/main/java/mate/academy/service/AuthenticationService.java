e431b532c33ed0d5ded1af7a79e9313cbab2fab3
John <pandastars535@gmail.com>
2023-11-25 14:31:21 -0500

    added ternary operator to solution made clean package check

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,9 +16,6 @@ public class AuthenticationService {
 
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        if (user == null) {
-            return false;
-        }
-        return user.getPassword().equals(password);
+        return user == null ? false : user.getPassword().equals(password);
     }
 }
