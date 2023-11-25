88ab0368ac9486e62ad5ad51bb19dabd6c4964e0
Yura Uniyat <yurauniyat123@gmail.com>
2023-11-25 12:50:45 +0200

    Homework jv-oop-lesson-0 .

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,16 +1,17 @@
 package mate.academy.service;
 
+import java.util.Optional;
+import mate.academy.model.User;
+
 public class AuthenticationService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
     public boolean login(String email, String password) {
-        return false;
+        UserService userService = new UserService();
+        Optional<User> byEmail = userService.findByEmail(email);
+        Optional<User> byPassword = userService.findByPassword(password);
+        if (byEmail.isEmpty() || byPassword.isEmpty()) {
+            return false;
+        }
+        return email.equals(byEmail.get().getEmail())
+                && password.equals(byPassword.get().getPassword());
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,20 +1,29 @@
 package mate.academy.service;
 
+import java.util.Optional;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
-    /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
-     */
-    public User findByEmail(String email) {
-        return null;
+    public Optional<User> findByEmail(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<User> findByPassword(String password) {
+        for (User user : users) {
+            if (user.getPassword().equals(password)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
     }
 }
