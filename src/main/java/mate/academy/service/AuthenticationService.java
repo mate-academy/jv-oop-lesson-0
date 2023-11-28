68a6cb821f24dcae1c77f34db4435cabecfde0dc
Tatiana <ttvv1801@gmail.com>
2023-11-28 21:04:04 +0100

    WhitespaceAround

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,9 +5,8 @@ import mate.academy.model.User;
 public class AuthenticationService {
 
     public boolean login(String email, String password) {
-
-        mate.academy.service.UserService userService=new UserService();
-        User user=userService.findByEmail(email);
+        mate.academy.service.UserService userService = new UserService();
+        User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
