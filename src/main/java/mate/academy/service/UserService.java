74dcd07401aae2298b81ed8d0ca28e9b6b4c711c
Kate Drevilo <rabmargo@gmail.com>
2023-12-04 16:38:36 +0200

    Fixed methods

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -7,8 +7,14 @@ public class UserService {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
+    /**
+     * Find user by email. All users are stored in <code>private static final User[] users</code>
+     * @param email - the input parameter
+     * @return - user if his email is equal to passed email.
+     * Return <code>null</code> if there is no suitable user
+     */
 
-    public static User findByEmail(String email) {
+    public User findByEmail(String email) {
         for (User user: users) {
             if (user.getEmail().equals(email)) {
                 return user;
