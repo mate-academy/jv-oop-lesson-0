bb092b9e69667b9040b0b1a450843be3fcfb3883
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-06 12:58:55 +0200

    added new AuthenticationService and UserService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,10 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService user = new UserService();
+        if (user.findByEmail(email) != null) {
+            return user.findByEmail(email).getPassword().equals(password);
+        }
         return false;
     }
 }
