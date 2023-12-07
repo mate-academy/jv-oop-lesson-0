15a2926e8034f1a82b92a31d267aa422c6771816
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-07 18:13:38 +0200

    changed AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,11 +1,14 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
 
     public boolean login(String email, String password) {
-        UserService user = new UserService();
-        if (user.findByEmail(email) != null) {
-            return user.findByEmail(email).getPassword().equals(password);
+        UserService users = new UserService();
+        User user = users.findByEmail(email);
+        if (user != null) {
+            return user.getPassword().equals(password);
         }
         return false;
     }
