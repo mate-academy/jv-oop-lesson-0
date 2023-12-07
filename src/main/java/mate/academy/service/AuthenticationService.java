89d6a9d11ff11001cffbf4841e22fec38c073036
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-07 15:22:16 +0200

    add AuthenticationService UserService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,16 +1,12 @@
 package mate.academy.service;
 
 public class AuthenticationService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+
     public boolean login(String email, String password) {
+        UserService user = new UserService();
+        if (user.findByEmail(email) != null) {
+            return user.findByEmail(email).getPassword().equals(password);
+        }
         return false;
     }
 }
