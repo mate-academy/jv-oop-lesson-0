d9bd7f4785b11db1eb2de24d8e5c3cd0363ac4d5
Anna Suprun <evgenevnaanna989@gmail.com>
2023-12-11 14:02:49 +0200

    made userService class field, changed return in login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,8 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private UserService userService = new UserService();
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -13,11 +15,9 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
-        User user = new UserService().findByEmail(email);
-        if (user != null) {
-            return user.getPassword().equals(password);
-        }
-        return false;
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
