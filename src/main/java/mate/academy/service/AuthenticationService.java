7f128bd72d659d7c4764a537b534c08aef8416d2
Yuliia <innishastudy@gmail.com>
2023-12-17 19:35:54 +0200

    added private to userService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,7 +12,8 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    UserService userService = new UserService();
+    private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
 
