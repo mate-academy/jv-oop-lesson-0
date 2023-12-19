c475bb3fb3663f78f9de628362029f6605ee60c8
Maksym Holubitskyi <golub1718@gmail.com>
2023-12-19 14:12:00 +0200

    Fixed method in class AuthenticationService.java - replaced initialisation of User instance location

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,11 +12,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    private UserService userService = new UserService();
+    private final UserService userService = new UserService();
 
     public boolean login(String email, String password) {
-        User user = null;
-        return (user = userService.findByEmail(email)) != null
-                && password.equals(user.getPassword());
+        User user = userService.findByEmail(email);
+        return user != null && password.equals(user.getPassword());
     }
 }
