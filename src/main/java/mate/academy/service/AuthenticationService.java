f454c091a93e2468bd870391afb467e23a81d6a0
NekitCool1 <gnnikita97@gmail.com>
2023-12-23 14:18:47 +0200

    fixed findByEmail method in UserService final version

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,8 +12,10 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         UserService userService = new UserService();
+        if (userService.findByEmail(email) == null) {
+            return false;
+        }
         return userService.findByEmail(email).getEmail().equals(email)
-                && userService.findByEmail(email).getPassword().equals(password)
-                && userService.findByEmail(email) != null;
+                && userService.findByEmail(email).getPassword().equals(password);
     }
 }
