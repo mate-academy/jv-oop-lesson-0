2013fef8fb8ad37e3f927eed662450f66207830f
Oleksandr <strelecaleksand@gmail.com>
2023-12-28 15:06:14 +0200

    fixed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -19,7 +19,7 @@ public class AuthenticationService {
         if (currentuser.findByEmail(email) != null) {
             User user = new User(currentuser.findByEmail(email).getEmail(),
                     currentuser.findByEmail(email).getPassword());
-            if (user.getPassword().equals(password) && user.getPassword().equals(password)) {
+            if (user.getPassword().equals(password)) {
                 return true;
             }
         }
