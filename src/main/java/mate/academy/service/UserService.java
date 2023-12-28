cb9696a334e8ecfb0085bc5596d3021a60ee8eac
Serhii Nosulia <sergeynosylya@gmail.com>
2023-12-29 00:17:27 +0200

    added login and findByEmail implementation

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,12 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        User userByEmail = null;
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return userByEmail = user;
+            }
+        }
         return null;
     }
 }
