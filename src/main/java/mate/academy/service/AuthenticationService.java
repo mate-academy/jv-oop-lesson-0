73f23ecac5b0da874fcab8f2b2d0189039516ec9
global source <danmaksimuk@gmail.com>
2023-12-29 11:31:44 +0100

    fixed problems which were specified as a result of checking the Checkstyle code style

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,10 +3,10 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-   private final UserService userService = new UserService();
+    private final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-
         return user != null && user.getPassword().equals(password);
     }
 }
