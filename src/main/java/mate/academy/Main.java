9fb76ed738fc17dd0a6d92f455de61bc6fff159a
Liza <lizabalashova7@gmail.com>
2023-12-30 11:33:46 +0200

    fix_1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static final AuthenticationService authenticationService = new AuthenticationService();
 
     public static void main(String[] args) {
-        test("bob@i.ua", "1234",  false);
+        test("bob@i.ua", "1234", false);
         test("alice@i.ua", "1234", false);
         test("john@i.ua", "1234", false);
         test("bob@i.ua", "qwerty", false);
