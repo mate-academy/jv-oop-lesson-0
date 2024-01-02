a1c838d7eb1818faa49d235e28d3e7ad5cd1a01a
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-02 17:54:09 +0200

    fixed the solution of oop-lesson-0

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,11 +15,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User findUser = userService.findByEmail(email);
-        if (findUser != null) {
-            if (findUser.getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
+        return (findUser != null && (findUser.getPassword().equals(password))) ? true : false;
     }
 }
