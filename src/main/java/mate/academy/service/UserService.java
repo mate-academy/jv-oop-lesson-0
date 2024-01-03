51580a5e4170f4dea98896cd3051f632bfe9e2f6
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-03 15:43:10 +0200

    added fixes after re-review

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,7 +15,7 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        for (User item: users) {
+        for (User item : users) {
             if (item.getEmail().equals(email)) {
                 return item;
             }
