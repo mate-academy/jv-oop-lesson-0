afbf5bb9c053d61769ac745ac625a9da76fab7db
Bohdan <bohdan2704@gmail.com>
2024-01-05 22:31:20 +0200

    initial commit

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -14,6 +14,7 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
+
     public User findByEmail(String email) {
         for (User currentUser : users) {
             if (currentUser.getEmail().equals(email)) {
