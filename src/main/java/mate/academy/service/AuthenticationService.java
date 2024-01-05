e14ff37160b180f10bfa4e92d6060aeb22cad26c
misha-levko <mykhailo.levko@gmail.com>
2024-01-04 20:33:23 -0500

    Added UserService field to AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,12 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private final UserService userService;
+
+    public AuthenticationService() {
+        this.userService = new UserService();
+    }
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -12,8 +18,9 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
-        User user = UserService.findByEmail(email);
+        User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
