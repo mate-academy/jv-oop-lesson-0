4adffcd8d4b580a95325f2484357a7921b08ee78
Vlad Postoiuk <vladpostoiuk411@gmail.com>
2024-01-05 18:47:55 +0200

    I tried to correct all your comments that you wrote to my pull request

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,28 +1,13 @@
 package mate.academy.service;
 
+// Assuming User class is in mate.academy.model package
+import mate.academy.model.User;
+
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
-
-    public static class User {
-        private final String email;
-        private final String password;
-
-        public User(String email, String password) {
-            this.email = email;
-            this.password = password;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-    }
 
     public User findByEmail(String email) {
         for (User user : users) {
@@ -33,3 +18,4 @@ public class UserService {
         return null;
     }
 }
+
