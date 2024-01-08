ee98d748e6d54374a60a19db9e15be41f5edc9a9
NekitCool1 <gnnikita97@gmail.com>
2024-01-08 13:39:49 +0200

    fixed all mistakes from comments

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,21 +1,16 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+    private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        if (userService.findByEmail(email) == null) {
+        User user = userService.findByEmail(email);
+        if (user == null) {
             return false;
         }
-        return userService.findByEmail(email).getEmail().equals(email)
-                && userService.findByEmail(email).getPassword().equals(password);
+        return user.getEmail().equals(email)
+                && user.getPassword().equals(password);
     }
 }
