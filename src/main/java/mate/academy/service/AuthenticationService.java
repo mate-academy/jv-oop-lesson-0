7af9d411c21729996df67379892a11f87c12d42e
moskalenkosergii <moskalenko.s.work@gmail.com>
2024-01-08 20:53:40 +0100

    moved services to the parent class

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,8 +12,9 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    UserService useService = new UserService();
+
     public boolean login(String email, String password) {
-        UserService useService = new UserService();
         User user = useService.findByEmail(email);
 
         if (user == null) {
