6d9425af4e6ff5c2623945c5c22d5aa3f32cb5ac
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2024-01-14 14:38:13 +0200

    fixed issues

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -8,15 +8,9 @@ public class UserService {
             new User("alice@i.ua", "1234")
     };
 
-    /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
-     */
     public User findByEmail(String email) {
         for (int i = 0; i < users.length; i++) {
-            if (users[i].getEmail() == email) {
+            if (users[i].getEmail().equals(email)) {
                 return users[i];
             }
         }
