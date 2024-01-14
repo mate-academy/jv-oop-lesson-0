c2acea25ebfbf91f776c9f92d4c9a15b48adcf37
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2024-01-14 14:32:18 +0200

    Fulfilled classes AuthenticationService and UserService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,8 +11,10 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     private static final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        if (userService.findByEmail(email) != null && userService.findByEmail(email).getPassword() == password) {
+        if (userService.findByEmail(email) != null
+                && userService.findByEmail(email).getPassword() == password) {
             return true;
         }
         return false;
