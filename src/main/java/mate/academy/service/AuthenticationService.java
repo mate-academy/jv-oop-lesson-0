65434a89cad07af75a9a3c8e812c66e3c25181f6
gulini32 <gulini32@gmail.com>
2024-01-17 14:21:45 +0200

    hw

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,6 +13,7 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
         User user = UserService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
