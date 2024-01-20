3f385e57b6017926374e7e704afa69d289866ef7
nastasiiaKulinich <kussik2004@gmail.com>
2024-01-20 16:02:20 +0100

    in login method used ternary operator instead of if construction

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -17,9 +17,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
 
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        return (user != null && user.getPassword().equals(password)) ? true : false;
     }
 }
