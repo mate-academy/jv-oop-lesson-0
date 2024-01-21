2a75edfcfb5db9b285aba64f43bb8d3f6a519447
Dmytro Nedashkovskyi <dmitrynedashkovsky@gmail.com>
2024-01-21 15:56:04 +0200

    make user a private field

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,13 +3,13 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private User user;
 
     public boolean login(String email, String password) {
         UserService userService = new UserService();
-        // Find the user by email
-        User user = userService.findByEmail(email);
 
-        // Check if the user exists and the provided password matches the user's password
-        return user != null && user.getPassword().equals(password);
+        this.user = userService.findByEmail(email);
+
+        return this.user != null && this.user.getPassword().equals(password);
     }
 }
