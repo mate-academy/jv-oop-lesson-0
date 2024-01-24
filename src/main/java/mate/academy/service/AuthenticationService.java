e5ed104f4f6c44b60a2e5803ceea541b1396cde5
Mykola <mykolamalovanchuk@gmail.com>
2024-01-24 02:54:26 +0200

    solved task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService userService = new UserService();
+
+        if (userService.findByEmail(email) != null
+                && userService.findByEmail(email).getPassword() == password) {
+            return true;
+        }
         return false;
     }
 }
