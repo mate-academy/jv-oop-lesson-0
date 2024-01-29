ec8b95b0e0c1ba1b01e8794d1b9eb1e17ac12bae
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-01-29 15:25:06 +0200

    moved the UserService and used ternary operator

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private final UserService userService = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -12,12 +13,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        return (user != null && user.getPassword().equals(password)) ? true : false;
+
     }
 }
