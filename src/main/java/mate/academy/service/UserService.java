6ff40f4a1ac3462ab6c13b98daf5b2bd4c026c8f
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-01-30 17:37:47 +0200

    //add comment )

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -14,6 +14,8 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
+    
+    //find user by email by using for each cycle
     public User findByEmail(String email) {
         for (User user : users) {
             if (email == user.getEmail()) {
