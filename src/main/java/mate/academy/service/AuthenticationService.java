1b81f44cb624c37f4161cb43e28292c234ffc3af
Mykola <mykolamalovanchuk@gmail.com>
2024-01-30 17:25:45 +0200

    change position of new UserService()

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,14 +5,16 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
+     *
      * @param email - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    public boolean login(String email, String password) {
-        UserService userService = new UserService();
 
+    UserService userService = new UserService();
+
+    public boolean login(String email, String password) {
         if (userService.findByEmail(email) != null
                 && userService.findByEmail(email).getPassword() == password) {
             return true;
