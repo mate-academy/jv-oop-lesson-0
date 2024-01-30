475b2468f9a51429a5712dfd8a907838ae01ae78
Mykola <mykolamalovanchuk@gmail.com>
2024-01-30 17:30:11 +0200

    declare userService as private variable

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,14 +5,13 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     *
      * @param email - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
 
-    UserService userService = new UserService();
+    private UserService userService = new UserService();
 
     public boolean login(String email, String password) {
         if (userService.findByEmail(email) != null
