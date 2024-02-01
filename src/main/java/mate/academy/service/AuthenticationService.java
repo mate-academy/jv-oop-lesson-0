7781b5a24a61de122718ffc6890ab2fc66416070
Oleksandr Farion <sashafarion@gmail.com>
2024-02-01 13:37:13 +0200

    made var user private

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,7 +3,11 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-    User user;
+    private User user;
+
+    public AuthenticationService() {
+        this.user = null;
+    }
 
     public boolean login(String email, String password) {
         user = new UserService().findByEmail(email);
