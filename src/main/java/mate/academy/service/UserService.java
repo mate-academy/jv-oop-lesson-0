51d0b7a5e03c740d5da4d1d06dfb9387dffcb0b3
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-02-03 15:02:10 +0200

    Update UserService.java
    Without static modifier does not pass checkstyle check

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -18,8 +18,8 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
-        for (User user :users) {
+    public User static findByEmail(String email) {
+        for (User user : users) {
             if (user.getEmail().equals(email)) {
                 return user;
             }
