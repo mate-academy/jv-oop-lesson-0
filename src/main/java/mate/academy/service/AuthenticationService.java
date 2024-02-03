4d8c4c56c9a879e7f03f8c48a1888fd2fa3d2b1c
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-02-03 14:44:05 +0200

    Update AuthenticationService.java
    added new variable for service method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private final UserService userService = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -13,7 +14,7 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        User user = UserService.findByEmail(email);
+        User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
