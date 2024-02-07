3d176b93e6d706cc92cf5b0b4a49211177eb2013
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-07 13:21:45 +0200

    Don't call UserService every time when I call method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    UserService userservice = new UserService();
     /*****
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -13,8 +14,6 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    UserService userservice = new UserService();
-
     public boolean login(String email, String password) {
         User user = userservice.findByEmail(email);
         return user != null && user.getPassword().equals(password);
