8073776bb81a8eccc0aeb050b49ae58fe2471b8b
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-07 13:06:28 +0200

    Revert "fixed some problems with style"
    This reverts commit 9f6e7f3e8a6a6ae6fd1026d13d09a69e402aca19.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-    /**
+    /*****
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
@@ -13,8 +13,8 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    UserService userservice = new UserService();
     public boolean login(String email, String password) {
+        UserService userservice = new UserService();
         User user = userservice.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
