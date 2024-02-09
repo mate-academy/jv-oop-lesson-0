f617fdfd26058bc2d9cc85bf48f105af69dee0ae
artemklishch <artemklishch@gmail.com>
2024-02-09 06:02:41 +0200

    removed extra expression

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,6 +16,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-        return user != null ? user.getPassword() == password : false;
+        return user != null && user.getPassword() == password;
     }
 }
