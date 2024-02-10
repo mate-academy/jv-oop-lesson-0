3ca092ba53a93e323545a3e8ef8a7ea35d880d1a
a1111 <okhrymovych11@gmail.com>
2024-02-10 20:12:24 +0200

    First Commit

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,13 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
+    private static final UserService userService = new UserService();
+
+    public AuthenticationService() {
+    }
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -11,6 +18,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        User userByEmail = userService.findByEmail(email);
+        if (userByEmail != null) {
+            return userByEmail.getPassword().equals(password);
+        } else {
+            User user = new User(email, "password");
+            return user.getPassword().equals(password);
+        }
     }
 }
