fd9fe703664e8ad09d288e4190c06b116f3e5ae3
Kateryna-Takhtai <ktakhtay22@gmail.com>
2024-02-13 22:04:46 +0400

    userService is initialized

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,7 +3,7 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-    private UserService userService;
+    private UserService userService = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
