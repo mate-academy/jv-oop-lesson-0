284b65cda656422e180817ea657872e5724ae541
maksym_korniev <maksimkarulet8@gmail.com>
2024-02-16 00:25:16 +0200

    Revert "UserService realized logic findByEmail method"
    This reverts commit d017d059076a63b068b6027776c97b808002f5a3.

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -1,6 +1,5 @@
 package mate.academy.service;
 
-import java.util.Arrays;
 import mate.academy.model.User;
 
 public class UserService {
@@ -16,9 +15,6 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return Arrays.stream(users)
-                .filter(user -> user.getEmail().equals(email))
-                .findFirst()
-                .orElse(null);
+        return null;
     }
 }
