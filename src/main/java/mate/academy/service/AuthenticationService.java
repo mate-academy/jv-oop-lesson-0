a2f4493b4d7c7ec1a6c694f05cc900d0892a817d
illia riaboi <riaboy94@gmail.com>
2024-02-17 02:37:24 +0200

    Solution

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import mate.academy.model.User;
 
 public class AuthenticationService {
     private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
         User user = UserService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
