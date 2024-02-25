41d1dd6cdc73550ee25d991e14235bcc9d8cdbb1
Fokin <Fokin1@ukr.net>
2024-02-25 12:50:48 +0100

    fixed method login in class AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,24 +3,20 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private static final UserService userService = new UserService();
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        User byEmail = userService.findByEmail(email);
-        if (byEmail != null) {
-            if (byEmail.getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
+    public boolean login(String email, String password) {
+        User userEmail = userService.findByEmail(email);
+        return userEmail != null && userEmail.getPassword().equals(password);
+    }
 }
