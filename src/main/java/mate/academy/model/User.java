11f7c0e0ed8558ceb97258a607ee22652da09bec
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-03-01 11:01:12 +0200

    solve the tusk

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -13,7 +13,15 @@ public class User {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
