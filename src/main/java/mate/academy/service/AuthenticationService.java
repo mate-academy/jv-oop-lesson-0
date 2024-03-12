0d878d190f6fe22c81807146bee282add5e21bb0
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2024-03-12 20:37:23 +0200

    small fix

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,7 +16,6 @@ public class AuthenticationService {
 
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        return user != null ? password.equals(user.getPassword())
-                : false;
+        return user != null && password.equals(user.getPassword());
     }
 }
