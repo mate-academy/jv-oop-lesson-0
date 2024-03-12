70df7df8bff5f553532acdfcc10c9458193686e8
Igor Glebov <igor.glebov0312@gmail.com>
2024-03-12 14:08:06 +0200

    Added authentication methods

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,6 +1,11 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
+    private static String passwordUser;
+    private static String emailUser;
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -10,7 +15,16 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private final User[] userService = UserService.getUsers();
+
     public boolean login(String email, String password) {
+        for (User user : userService) {
+            if (email.equals(user.getEmail()) && password.equals(user.getPassword())) {
+                return true;
+
+            }
+
+        }
         return false;
     }
 }
