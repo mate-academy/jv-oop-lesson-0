efa310ffa31c76f225c096816969488991877991
Ivan Kusurhashev <ivankusur@gmail.com>
2024-03-17 20:19:22 +0100

    finished the task jv-oop-lesson-0 "Working with services"

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,18 +3,25 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        return null;
+        User returnUser = new User("USER NOT CONTAIN", "USER NOT CONTAIN");
+        for (User user : users) {
+            if (email.equals(user.getEmail())) {
+                returnUser = user;
+            }
+        }
+        return returnUser;
     }
 }
