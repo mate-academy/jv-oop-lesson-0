d90c67e56600a96421762f680e255e8d5340faf3
Igor Glebov <igor.glebov0312@gmail.com>
2024-03-19 13:58:31 +0200

    Added fixes for extends and methods

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,7 +3,6 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
-
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -13,10 +12,12 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    private final UserService userService; {
+
+    private final UserService userService;
+
+    public AuthenticationService() {
         this.userService = new UserService();
     }
-
 
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
