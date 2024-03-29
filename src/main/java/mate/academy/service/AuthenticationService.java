eb4ef7b09685611ef11d4d68276c7b1f549f3d98
Ievgenii Kazanov <159741133+Jacalius@users.noreply.github.com>
2024-03-29 09:39:07 -0600

    spaces removed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -14,6 +14,6 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         User user = UserService.findByEmail(email);
-         return user != null && user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
