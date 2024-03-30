a4219ab2ecb8d3feed84552b4b220445516e5e1c
Ievgenii Kazaov <Jacalius1@gmail.com>
2024-03-30 11:51:57 -0600

    made the class public User findByEmail not static

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,10 +13,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        User user = UserService.findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        UserService user1 = new UserService();
+        User user = user1.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
