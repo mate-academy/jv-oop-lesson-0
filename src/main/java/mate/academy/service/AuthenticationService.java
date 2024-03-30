6e1c8f568bb2199b4bd0452b7ef8632b6272fe3b
Ievgenii Kazaov <Jacalius1@gmail.com>
2024-03-30 12:49:31 -0600

    moved announced UserService on a class level field, changed name

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,9 +12,14 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private UserService userService = new UserService();
+
+    public UserService getUserService() {
+        return userService;
+    }
+
     public boolean login(String email, String password) {
-        UserService user1 = new UserService();
-        User user = user1.findByEmail(email);
+        User user = userService.findByEmail(email);
         return user != null && user.getPassword().equals(password);
     }
 }
