722db090d870073465ba0915e4c0d6da1a232123
SviatoslavTataryn <www.t.svyatoslav55@gmail.com>
2024-03-30 17:43:04 +0100

    Implement login method in AuthenticationService and findByEmail method in UserService.

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -10,11 +10,20 @@ public class UserService {
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
+     *
+     * @param email    - the input parameter
+     * @param password - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
+
+    public User findByEmail(String email, String password) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getEmail().equals(email) && users[i]
+                    .getPassword().equals(password)) {
+                return users[i];
+            }
+        }
         return null;
     }
 }
