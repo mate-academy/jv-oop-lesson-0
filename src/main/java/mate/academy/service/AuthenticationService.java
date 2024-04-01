3ad2704c958496e433fa916d924d40bc8debb202
Kateryna <keitkat98@gmail.com>
2024-04-01 10:58:09 +0300

    removed the unnecessary lines after review

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -22,5 +22,4 @@ public class AuthenticationService {
         }
         return false;
     }
-
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,7 +15,6 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-
         for (int i = 0; i < users.length; i++) {
             if (users[i].getEmail().equals(email)) {
                 return users[i];
