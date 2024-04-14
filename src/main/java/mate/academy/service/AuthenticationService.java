a7e93824016829dcff58649030afba81d9b023b4
Anton <antchaycka@gmail.com>
2024-04-14 19:24:00 +0200

    made syntax fixes

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -7,7 +7,8 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
+     *
+     * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
@@ -19,6 +20,6 @@ public class AuthenticationService {
 
         return user != null && password.equals(user.getPassword());
 
-        }
     }
+}
 
