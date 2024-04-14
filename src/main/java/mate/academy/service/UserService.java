27791252c6cbc9ac0b12a15e265140aac7398a8d
Misha Havryliuk <misharob31415@gmail.com>
2024-04-14 13:14:28 +0300

    change

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -9,12 +9,17 @@ public class UserService {
     };
 
     /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
+     * Find user by email.
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
+     * Return null if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
         return null;
     }
 }
