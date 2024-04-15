44edcbc75d88a0e7dace47d4b19415e0fcb9150e
Vlad Poyda <playeeer.steam@gmail.com>
2024-04-15 18:00:52 +0300

    Add private userService

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,13 +3,14 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
-    private static final User[] users = new User[] {
+    private static final User[] users = new User[]{
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234")
     };
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
+     *
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
@@ -19,7 +20,8 @@ public class UserService {
             if (userEmail.equals(email)) {
                 return userEmail;
             }
-
+        }
         return null;
     }
 }
+
