706a19574543cbc192ba55ad1f7aa4a29ba87900
Ivan Demchenko <experywow@gmail.com>
2024-04-16 17:47:45 +0100

    Added an emptyline between comments and object "userService" for more readability. Simplified expression on line 20.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,10 +12,11 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     private UserService userService = new UserService();
 
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        return (user != null && user.getPassword().equals(password)) ? true : false;
+        return user != null && user.getPassword().equals(password);
     }
 }
