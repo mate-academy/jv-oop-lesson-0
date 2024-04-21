9c610af699e03bada4fb3fa67f91ec7a1bfcca87
Wasja Zejkan <vasazejkan5@gmail.com>
2024-04-21 19:06:06 +0300

    Refactored a login(email, password) method in the AuthenticationService class

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,9 +16,6 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        if (user == null) {
-            return false;
-        }
-        return user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
