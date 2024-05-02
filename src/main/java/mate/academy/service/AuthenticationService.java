6b10b08d5218e7dfe58407b6716a280c6c258aa0
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-05-02 22:49:19 +0200

    Delete needless method(getUsers) from UserService, and fix AuthenticationService login method.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,6 +3,12 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private final UserService userService;
+
+    public AuthenticationService() {
+        this.userService = new UserService();
+    }
+
     /**
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
@@ -14,12 +20,8 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        for (User user : UserService.getUsers()) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                return true;
-            }
-        }
+        User user = userService.findByEmail(email);
 
-        return false;
+        return user != null && user.getPassword().equals(password);
     }
 }
