613d2d2e0b27f00bfc00fb20bf74544f14caa89f
Nikita Kravtsov <deez999deez@gmail.com>
2024-06-11 14:05:25 +0300

    avoid calling method "findByEmail" twice with saved email from "findByEmail return" in "user" variable.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -13,9 +15,7 @@ public class AuthenticationService {
     private final UserService userService = new UserService();
 
     public boolean login(String email, String password) {
-        if (userService.findByEmail(email) != null) {
-            return userService.findByEmail(email).getPassword().equals(password);
-        }
-        return false;
+        User user = userService.findByEmail(email);
+            return user != null && user.getPassword().equals(password);
     }
 }
