92dea691937ce63c8a9b9191e57ce1f7757d465d
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2024-06-19 21:36:28 +0300

    created a UserService object by calling the findByEmail method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,11 +13,12 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
     public boolean login(String email, String password) {
         User qw = userService.findByEmail(email);
-      if (qw != null && qw.getPassword().equals(password)) {
-          return true;
-      }
-      return false;
+        if (qw != null && qw.getPassword().equals(password)) {
+            return true;
+        }
+        return false;
     }
 }
