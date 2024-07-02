2ed2d554e74aa42d89f48f494a0ed68e5d78ab1f
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2024-07-02 19:45:38 +0300

    added fixes after review

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,8 +15,8 @@ public class AuthenticationService {
      */
 
     public boolean login(String email, String password) {
-        User qw = userService.findByEmail(email);
-        if (qw != null && qw.getPassword().equals(password)) {
+        User user = userService.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
             return true;
         }
         return false;
