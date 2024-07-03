2c6594a56c2a9829aa1a919e4f7a1d6eaaf7800b
Epikano <epikano@gmail.com>
2024-07-03 20:29:53 +0300

    Added jv-oop-lesson-0 solution.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -10,7 +12,11 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        return false;
+        User user = userService.findByEmail(email);
+
+        return !(user == null || !password.equals(user.getPassword()));
     }
 }
