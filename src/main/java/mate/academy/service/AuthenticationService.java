4c5499083b969e6e20a44fa96dbc37097815eda6
Artem Stadnik <artem.stadnik.contact@gmail.com>
2024-07-08 13:17:39 +0300

    solved task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,13 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        return false;
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+
+        if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
+            return false;
+        }
+
+        return user != null && user.getPassword().equals(password);
     }
 }
