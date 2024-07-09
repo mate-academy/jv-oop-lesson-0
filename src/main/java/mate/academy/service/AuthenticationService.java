8f04dbe8346879f9dab655328d2bf529fd77d632
Petro Shchur <pepe2.98.12@icloud.com>
2024-07-09 09:18:55 -0400

    Fixed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,7 +13,7 @@ public class AuthenticationService {
      * Imagine that some user wants to login to your site.
      * You should check if user credentials (login and password) are valid or not.
      * All users are stored in <code>UserService</code> class.
-     *
+     * add
      * @param email    - user's email
      * @param password - user's password
      * @return true if user by email exists and passed password is equal to user's password.
