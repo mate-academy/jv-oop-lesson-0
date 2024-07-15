af5c726007b7f215c8361cdb483679418cacb002
Igor Neminuschii <igorgorslav@gmail.com>
2024-07-15 21:40:05 +0300

    fixed login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -12,7 +14,7 @@ public class AuthenticationService {
      */
 
     public boolean login(String email, String password) {
-        return UserService.findByEmail(email) != null
-                && password.equals(UserService.findByEmail(email).getPassword());
+        User user = UserService.getInstance().findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,11 +3,16 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class UserService {
+    private static UserService instance;
+
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
             new User("alice@i.ua", "1234"),
             new User("john@i.ua", "6789")
     };
+
+    private UserService() {
+    }
 
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
@@ -15,12 +20,20 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public static User findByEmail(String email) {
+
+    public User findByEmail(String email) {
         for (int i = 0; i < users.length; i++) {
             if (email.equals(users[i].getEmail())) {
                 return users[i];
             }
         }
         return null;
+    }
+
+    public static UserService getInstance() {
+        if (instance == null) {
+            instance = new UserService();
+        }
+        return instance;
     }
 }
