1c10ef6a28cb0dc17805783cbd4b42fdd6dff663
Lilia <liliazuravel6@gmail.com>
2024-07-20 16:46:21 +0300

    created  login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,13 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService user = new UserService();
+        User found = user.findByEmail(email);
+        if (found != null) {
+            if (found.getPassword().equals(password)) {
+                return true;
+            }
+        }
         return false;
     }
 }
