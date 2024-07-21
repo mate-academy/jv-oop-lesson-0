8f15ec6d266fc8b6af084b46f5f8b39a5edcf0d3
Lilia <liliazuravel6@gmail.com>
2024-07-21 22:16:28 +0300

    added setter and made the userService variable private and moved outside the method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,9 +12,14 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private UserService userService = new UserService();
+
+    public UserService getUserService() {
+        return userService;
+    }
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        User userToCheck = userService.findByEmail(email);
+        User userToCheck = getUserService().findByEmail(email);
 
         return userToCheck != null && userToCheck.getPassword().equals(password);
     }
