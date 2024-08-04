fb9f0e3fdbb2d33b66952456d8cdf5c9b95f14d1
karma.misha <karma.misha@gmail.com>
2024-08-04 19:28:47 +0300

    fix login() method in AuthenticationService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -15,15 +15,6 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         UserService userService = new UserService();
         User user = userService.findByEmail(email);
-
-        if (user == null) {
-            return false;
-        }
-
-        if (password.equals(user.getPassword())) {
-            return true;
-        }
-
-        return false;
+        return user != null && user.getPassword().equals(password);
     }
 }
