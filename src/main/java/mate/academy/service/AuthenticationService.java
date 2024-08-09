7c57aa793d2bd299c4e4317a9ce07fbc62e837ee
natalia burtnyk <nataliaburtnyk@gmail.com>
2024-08-09 11:23:43 -0500

    Fixed remarks.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -12,16 +12,11 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+
+    private final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
         User user = userService.findByEmail(email);
-
-        if (user != null) {
-            if (user.getPassword().equals(password)) {
-                return true;
-            }
-        }
-
-        return false;
+        return userService.findByEmail(email) != null && user.getPassword().equals(password);
     }
 }
