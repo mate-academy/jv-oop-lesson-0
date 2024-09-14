bb9ec779d4dc5028e34b6047e3d94511e4085ca6
Heorhii Tsarenko <heorhii.tsarenko@gmail.com>
2024-09-14 13:52:34 +0300

    Done all task

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -10,7 +10,18 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private static final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
+        if (email == null || password == null) {
+            return false;
+        }
+
+        if (userService.findByEmail(email) != null
+                && password.equals(userService.findByEmail(email).getPassword())) {
+            return true;
+        }
+
         return false;
     }
 }
