cd18e080f4a964783a86275b140ea04626ba68aa
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-09-18 13:52:07 +0200

    Fixed checkstyle error

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,7 +13,7 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    final private UserService userService = new UserService();
+    private final UserService userService = new UserService();
 
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
