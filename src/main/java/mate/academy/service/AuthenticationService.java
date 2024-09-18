2ad08aedfc6c5d334be45adf18e6e88903960437
Heorhii Tsarenko <heorhii.tsarenko@gmail.com>
2024-09-18 18:02:19 +0300

    Change a few mistakes delete static from userservice field make variable to findByMail method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -10,18 +12,10 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
-    private static final UserService userService = new UserService();
+    private final UserService userService = new UserService();
 
     public boolean login(String email, String password) {
-        if (email == null || password == null) {
-            return false;
-        }
-
-        if (userService.findByEmail(email) != null
-                && password.equals(userService.findByEmail(email).getPassword())) {
-            return true;
-        }
-
-        return false;
+        User user = userService.findByEmail(email);
+        return user != null && password.equals(user.getPassword());
     }
 }
