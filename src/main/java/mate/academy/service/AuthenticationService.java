722f0e428c655f89e0e97557f2af67eed8651b9a
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-09-18 13:38:25 +0200

    Used UserService at the class level and simplified the login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,11 +13,11 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    final private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        User user = new UserService().findByEmail(email);
-        if (user != null && user.getEmail().equals(email) && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        User user = userService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
+
     }
 }
