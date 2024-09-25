c4c27dda59a948ccc5584c6c62e83ddf8fbde562
Borovski Cergei <borovskicerg0001@gmaol.com>
2024-09-25 19:58:40 +0300

    created an instance of UserService at the class's level

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,8 +13,9 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
 
+    private UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
         User user = userService.findByEmail(email);
         return user != null && password.equals(user.getPassword());
     }
