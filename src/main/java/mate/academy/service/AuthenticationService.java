4473f53120c534c015a69edb8b48899806909848
Sasha Koval <kovalsana40@gmail.com>
2024-09-28 23:42:00 +0300

    Can you see now?

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -14,7 +14,6 @@ public class AuthenticationService {
      */
     private UserService userService = new UserService();
 
-
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
         if (user != null) {
