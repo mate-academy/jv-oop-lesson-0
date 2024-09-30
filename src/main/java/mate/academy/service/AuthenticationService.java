a4d550b8e347840479aaa5c31cba3c32804df580
Dymchenko, Mykola <mdymchen@estee.com>
2024-09-30 18:54:58 -0500

    Added solution to the task. Modified files: AuthenticationService and UserService

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,15 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService userService = new UserService();
+        User userByEmail = userService.findByEmail(email);
+
+        if (userByEmail != null) {
+            if (userByEmail.getEmail().equals(email)
+                    && userByEmail.getPassword().equals(password)) {
+                return true;
+            }
+        }
         return false;
     }
 }
