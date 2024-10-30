292cfd858e616186b78adf35d87c0fe13d376f60
Teslenko Daniil <burgalhashem@gmail.com>
2024-10-30 13:27:49 +0200

    implements methods needed

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,6 +13,12 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService userservice = new UserService();
+        User pass =  userservice.findByEmail(email);
+
+        if (pass != null && pass.getPassword().equals(password)) {
+            return true;
+        }
         return false;
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,6 +15,11 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
+        for (User us : users){
+            if (us.getEmail().equals(email)) {
+                return us;
+            }
+        }
         return null;
     }
 }
