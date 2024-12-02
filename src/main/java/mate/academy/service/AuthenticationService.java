f16e6016058c289ea5b3d893e7d5a4398d7bd797
Mirrox-cloud <beznosmaksim0102@gmail.com>
2024-12-02 17:24:11 +0200

    added fixes after review

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -6,14 +6,11 @@ public class AuthenticationService {
     private final UserService userService = new UserService();
 
     public boolean login(String email, String password) {
-        User testUser = userService.findByEmail(email);
-        if (testUser != null) {
-            return testUser.getPassword().equals(password);
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            return user.getPassword().equals(password);
         }
         return false;
     }
 
-    public UserService getUserService() {
-        return userService;
-    }
 }
