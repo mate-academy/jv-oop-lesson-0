136905baecb3efb7c0517f60b76ad904c5ac6352
Игорь <sokol.igor29@gmail.com>
2024-12-10 17:28:00 +0200

    made userService class-level variable

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,8 +3,9 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private final UserService userService = new UserService();
+
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
         User user = userService.findByEmail(email);
         if (user != null && user.getPassword().equals(password)) {
             return true;
