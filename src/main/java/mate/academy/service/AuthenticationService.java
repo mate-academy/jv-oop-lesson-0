23771d47d610f9522d24ae34fb345632edc9b8d6
Acid <deadfoxua1337@gmail.com>
2024-12-12 14:34:44 +0100

    ixtyle

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package mate.academy.service;
 
+import mate.academy.model.User;
+
 public class AuthenticationService {
     /**
      * Imagine that some user wants to login to your site.
@@ -11,8 +13,10 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        if (UserService.findByEmail(email) != null) {
-            return UserService.findByEmail(email).getPassword().equals(password);
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            return user.getPassword().equals(password);
         }
         return false;
     }
