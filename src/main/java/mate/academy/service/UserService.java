46d42b955ce021ff11254dd33d368062b352ee5f
FroGitHub <andrey8102310@gmail.com>
2024-12-19 17:50:17 +0200

    add verify

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -14,7 +14,12 @@ public class UserService {
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
-    public User findByEmail(String email) {
-        return null;
+    public static User findByEmail(String email) {
+        for (User u : users) {
+            if (email.equals(u.getEmail())) {
+                return u;
+            }
+        }
+        return new User("NoNe", "");
     }
 }
