bbb1ae7a6b61ac6f7287fcea03b42a8b923422b9
michal <mihacik228@gmail.com>
2025-01-21 17:04:58 +0100

    after check

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -2,7 +2,12 @@ package mate.academy.service;
 
 import mate.academy.model.User;
 
-public class AuthenticationService extends UserService {
+public class AuthenticationService  {
+    private final UserService userService;
+
+    public AuthenticationService() {
+        this.userService =  new UserService();
+    }
 
     /**
      * Imagine that some user wants to login to your site.
@@ -15,7 +20,7 @@ public class AuthenticationService extends UserService {
      */
 
     public boolean login(String email, String password) {
-        User user = findByEmail(email);
+        User user = userService.findByEmail(email);
         if (user != null && user.getPassword().equals(password)) {
             return true;
         }
