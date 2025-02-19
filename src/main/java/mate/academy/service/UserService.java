7da5f0ac1e9fbaa1efcc1a370a201e5418f4073e
kraievskyi-om <kraievskyi-om@ukrposhta.com>
2025-02-19 09:22:04 +0200

    checkstyle

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -6,7 +6,7 @@ import mate.academy.model.User;
 public class UserService {
     private static final User[] users = new User[] {
             new User("bob@i.ua", "1234"),
-            new User("alice@i.ua", "1234"),
+            new User("alice@i.ua", "1234")
     };
 
     /**
