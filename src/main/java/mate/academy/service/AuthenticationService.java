4118cf616bc2ffdec2afca0e24990b9f063bb1a0
Yulian Popovych <popovuch01@gmail.com>
2025-02-26 22:45:59 +0100

    update methods login and findByEmail

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -11,6 +11,13 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
+        UserService userService = new UserService();
+        if (userService.findByEmail(email) == null) {
+            return false;
+        } else if (userService.findByEmail(email).getEmail().equals(email)
+                && userService.findByEmail(email).getPassword().equals(password)) {
+            return true;
+        }
         return false;
     }
 }
