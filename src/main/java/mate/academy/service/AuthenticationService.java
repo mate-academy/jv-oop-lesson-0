3696ae0f1247355de9e5fffef96cf01e45e6a53b
Egor <llikojia162@gmail.com>
2025-04-26 20:15:26 +0200

    implemented `login` method in `AuthenticationService` class, implemented findByEmail` method in `UserService` class

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,16 +1,15 @@
 package mate.academy.service;
 
 public class AuthenticationService {
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     * @param email - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
+
     public boolean login(String email, String password) {
+
+        for (int i = 0; i < UserService.getUsers().length; i++) {
+            if (UserService.getUsers()[i].getEmail().equals(email)
+                    && UserService.getUsers()[i].getPassword().equals(password)) {
+                return true;
+            }
+        }
         return false;
     }
 }
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -8,13 +8,16 @@ public class UserService {
             new User("alice@i.ua", "1234")
     };
 
-    /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
-     */
+    public static User[] getUsers() {
+        return users;
+    }
+
     public User findByEmail(String email) {
+        for (int i = 0; i < UserService.getUsers().length; i++) {
+            if (UserService.getUsers()[i].getEmail().equals(email)) {
+                return UserService.getUsers()[i];
+            }
+        }
         return null;
     }
 }
